60f24a06e7406d07a4129181ad461b25bebcdda8
Hah-nna <hj08hn@gmail.com>
2024-12-08 16:04:44 +0900

    add : 찾을 쪽지 리스트 프래그먼트, 레이아웃 추가
    - 찾을 쪽지 리스트 프래그먼트, 레이아웃을 추가합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/SearchListFragment.kt
@@ -0,0 +1,31 @@
+package com.jeong.sesac.sai.ui.searchRegister.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.jeong.sesac.sai.R
+import com.jeong.sesac.sai.databinding.FragmentSearchListBinding
+import com.jeong.sesac.sai.util.BaseFragment
+
+class SearchListFragment : BaseFragment<FragmentSearchListBinding> (FragmentSearchListBinding::inflate) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchListBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            searchNoteDetailBtn.setOnClickListener {
+                findNavController().navigate(R.id.action_fragmentSearchList_to_fragmentSearchNoteDetail)
+            }
+        }
+    }
+}
