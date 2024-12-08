3beecd2e1bcd09593c989f9254b42e007c69d00b
Hah-nna <hj08hn@gmail.com>
2024-12-08 16:01:56 +0900

    add : 바코드스캔 프래그먼트 추가
    - 바코드 스캔 프래그먼트를 추가합니다
    - 찾은쪽지디테일로 이동하는 액션을 추가합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/BarcodeScannerFragment.kt
@@ -0,0 +1,31 @@
+package com.jeong.sesac.sai.ui.searchRegister.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.jeong.sesac.sai.R
+import com.jeong.sesac.sai.databinding.FragmentBarcodeScannerBinding
+import com.jeong.sesac.sai.util.BaseFragment
+
+class BarcodeScannerFragment : BaseFragment<FragmentBarcodeScannerBinding>(FragmentBarcodeScannerBinding::inflate) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentBarcodeScannerBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            barcodeScannerBtn.setOnClickListener {
+                findNavController().navigate(R.id.action_fragmentBarcodeScanner_to_fragmentFoundNoteDetail)
+            }
+        }
+    }
+}
