afd71a8ead8ef00b61a139e00f9b34a40535176f
Hah-nna <hj08hn@gmail.com>
2024-12-08 13:18:48 +0900

    edd : navigationRecentlyNotesDetail fragment
    - navigationRecentlyNotesDetailFragment를 생성합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/ui/home/RecentlyFoundNotesDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.jeong.sesac.sai.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jeong.sesac.sai.databinding.FragmentRecentlyFoundNotesDetailBinding
+import com.jeong.sesac.sai.util.BaseFragment
+
+class RecentlyFoundNotesDetailFragment : BaseFragment<FragmentRecentlyFoundNotesDetailBinding>(FragmentRecentlyFoundNotesDetailBinding::inflate){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecentlyFoundNotesDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+
+}
