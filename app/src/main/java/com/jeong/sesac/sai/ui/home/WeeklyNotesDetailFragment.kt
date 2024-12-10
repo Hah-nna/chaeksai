62e9ff0d18f486a34fb874ee2c3b4360bf3a8a6b
Hah-nna <hj08hn@gmail.com>
2024-12-11 03:50:25 +0900

    feat : add WeeklyNotesDetailFragment
    - WeeklyNotesDetailFragment을 추가합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/ui/home/WeeklyNotesDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.jeong.sesac.sai.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jeong.sesac.sai.databinding.FragmentWeeklyNoteDetailBinding
+import com.jeong.sesac.sai.util.BaseFragment
+
+class WeeklyNotesDetailFragment : BaseFragment<FragmentWeeklyNoteDetailBinding>(FragmentWeeklyNoteDetailBinding::inflate) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWeeklyNoteDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
