a85d2d31f5c75f965dd6bcddb02e6dceae44c8d5
Hah-nna <hj08hn@gmail.com>
2024-12-11 03:49:20 +0900

    design : gridlayout item decoration
    - item decoration을 추가합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/recycler/recentlyFoundNotesRecycler/RecyclerDecoration.kt
@@ -0,0 +1,31 @@
+package com.jeong.sesac.sai.recycler.recentlyFoundNotesRecycler
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerDecoration(
+    private val spanCount : Int,
+    private val spacing: Int
+) : RecyclerView.ItemDecoration() {
+
+    /** 개별 항목 데코레이션시 호출*/
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+
+        val position = parent.getChildLayoutPosition(view)
+        val column = position % spanCount
+
+        if(column == 0) {
+            outRect.right = spacing / 2
+        } else {
+            outRect.left = spacing / 2
+        }
+
+        outRect.bottom = spacing
+    }
+}
