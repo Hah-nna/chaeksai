e98fda65262008512771aae2b7e6e8cbda6379ac
Hah-nna <hj08hn@gmail.com>
2024-12-11 17:49:08 +0900

    rename : 프래그먼트 이름 수정
    - 리사이클러뷰 관련 프래그먼트의 이름을 수정합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/recentlyFoundNotesRecycler/RecentlyFoundNotesViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.jeong.sesac.sai.recycler.recentlyFoundNotesRecycler
-
-import androidx.recyclerview.widget.RecyclerView
-import com.jeong.sesac.sai.databinding.ItemRecentlyFoundNoteBinding
-
-class RecentlyFoundNotesViewHolder(val binding: ItemRecentlyFoundNoteBinding) : RecyclerView.ViewHolder(binding.root) {
-}
--- app/src/main/java/com/jeong/sesac/sai/recycler/weeklynotes/WeeklyNotesViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.jeong.sesac.sai.recycler.weeklynotes
-
-import androidx.recyclerview.widget.RecyclerView
-import com.jeong.sesac.sai.databinding.ItemWeeklyNotesBinding
-
-class WeeklyNotesViewHolder(val binding : ItemWeeklyNotesBinding) : RecyclerView.ViewHolder(binding.root) {
-}
