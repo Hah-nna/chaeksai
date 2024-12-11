b3ecb5f860bf3fe7b5702db296c8e71efb700cb9
Hah-nna <hj08hn@gmail.com>
2024-12-11 17:54:16 +0900

    feat : 찾기/등록 프래그먼트 추가
    - 카카오맵 뷰가 보일 프래그먼트 추가합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/ui/MapSearchRegisterFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/MapSearchRegisterFragment.kt
@@ -5,13 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.FragmentSearchRegisterBinding
+import com.jeong.sesac.sai.recycler.gridRecycler.GridNotesAdapter
 import com.jeong.sesac.sai.util.BaseFragment
+import com.jeong.sesac.sai.util.WeeklyNoteMockData
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+import java.lang.Exception
 
 class MapSearchRegisterFragment :
     BaseFragment<FragmentSearchRegisterBinding>(FragmentSearchRegisterBinding::inflate) {
+    private lateinit var registerNotedAdapter : GridNotesAdapter
+    private lateinit var findNotedAdapter : GridNotesAdapter
+    private lateinit var mapView: MapView
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +37,58 @@ class MapSearchRegisterFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            registerBtn.setOnClickListener {
-                findNavController().navigate(R.id.action_fragmentSearchRegister_to_fragmentRegisterDetail)
+
+        mapView = binding.mapView
+
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
             }
 
-            searchBtn.setOnClickListener {
-                findNavController().navigate(R.id.action_fragmentSearchRegister_to_fragmentSearchList)
+            override fun onMapError(err: Exception?) {
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(p0: KakaoMap) {
+            }
+        })
+
+        registerNotedAdapter = GridNotesAdapter { registerNote ->
+            val action = MapSearchRegisterFragmentDirections.actionFragmentSearchRegisterToFragmentRegisterDetail("새싹도서관")
+            findNavController().navigate(action)
+
+        }
+
+        findNotedAdapter = GridNotesAdapter { findNote ->
+            val action = MapSearchRegisterFragmentDirections.actionFragmentSearchRegisterToFragmentSearchList(
+                arrayOf(findNote)
+            )
+            findNavController().navigate(action)
+        }
+
+        with(binding) {
+            btnRegister.setOnClickListener {
+                val registerAction = MapSearchRegisterFragmentDirections.actionFragmentSearchRegisterToFragmentRegisterDetail("새싹도서관")
+                findNavController().navigate(registerAction)
+            }
+            btnFind.setOnClickListener {
+                val findAction = MapSearchRegisterFragmentDirections
+                    .actionFragmentSearchRegisterToFragmentSearchList(
+                        WeeklyNoteMockData.notesList.toTypedArray()
+                    )
+                findNavController().navigate(findAction)
             }
         }
+        registerNotedAdapter.submitList(WeeklyNoteMockData.notesList)
+        findNotedAdapter.submitList(WeeklyNoteMockData.notesList)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
     }
 }
