758327f478a94cffefe89226b383153b91c41c92
Hah-nna <hj08hn@gmail.com>
2024-12-11 17:47:20 +0900

    feat : 쪽지찾기 관련 프래그먼트 추가
    - 쪽지 찾기 관련 프래그먼트를 추가합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/SearchListFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/SearchListFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
-import com.jeong.sesac.sai.R
+import androidx.recyclerview.widget.GridLayoutManager
 import com.jeong.sesac.sai.databinding.FragmentSearchListBinding
+import com.jeong.sesac.sai.recycler.gridRecycler.GridNotesAdapter
+import com.jeong.sesac.sai.recycler.gridRecycler.GridRecyclerDecoration
+import com.jeong.sesac.sai.ui.home.RecentlyFoundNotesFragmentDirections
 import com.jeong.sesac.sai.util.BaseFragment
+import com.jeong.sesac.sai.util.WeeklyNoteMockData
 
 class SearchListFragment : BaseFragment<FragmentSearchListBinding> (FragmentSearchListBinding::inflate) {
-
+    private lateinit var searchListAdapter : GridNotesAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,10 +27,31 @@ class SearchListFragment : BaseFragment<FragmentSearchListBinding> (FragmentSear
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().navigateUp()
+        }
+        searchListAdapter = GridNotesAdapter { foundNote ->
+            val action =
+                RecentlyFoundNotesFragmentDirections.actionFragmentRecentlyFoundNotesToDetail(
+                    foundNote
+                )
+            findNavController().navigate(action)
+        }
+
         with(binding) {
-            searchNoteDetailBtn.setOnClickListener {
-                findNavController().navigate(R.id.action_fragmentSearchList_to_fragmentSearchNoteDetail)
+            rvGridNotesList.apply {
+                layoutManager = GridLayoutManager(requireContext(), 2)
+                addItemDecoration(GridRecyclerDecoration(2, 96))
+                adapter = this@SearchListFragment.searchListAdapter
             }
         }
+        searchListAdapter.submitList(WeeklyNoteMockData.notesList)
+
+
     }
 }
