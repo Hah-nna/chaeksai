66595579383b83d71dc5387eb846eeb97e55e21f
Hah-nna <hj08hn@gmail.com>
2024-12-11 17:45:30 +0900

    fix : 금주의 쪽지 관련 데이터클래스
    - 금주의 쪽지 관련 데이터클래스를 추가합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/util/WeeklyNotesInfo.kt
+++ app/src/main/java/com/jeong/sesac/sai/util/WeeklyNotesInfo.kt
@@ -8,12 +8,7 @@ data class WeeklyNotesInfo(
     val id : String = "0",
     val date : String,
     val note_content : String,
-    val hint_img: Int = -1,
+    val hint_img: Int,
     val library: String,
     val hints : List<String>
 ) : Parcelable
-
-// 사진 힌트(사진)
-//날짜(쪽지 등록된 날짜)
-//도서관명
-//힌트 List<String>
