8e0558bb225b78d2b40136c70c9c221db68827e3
Hah-nna <hj08hn@gmail.com>
2024-12-12 17:54:14 +0900

    fix : arg 수정
    - arg 부분을 추가합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/SearchNoteDetailFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/searchRegister/search/SearchNoteDetailFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import androidx.navigation.fragment.findNavController
-import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.FragmentSearchNoteDetailBinding
 import com.jeong.sesac.sai.util.BaseFragment
 
 class SearchNoteDetailFragment : BaseFragment<FragmentSearchNoteDetailBinding>(FragmentSearchNoteDetailBinding::inflate){
+
+    private val args : SearchNoteDetailFragmentArgs by navArgs()
+    private val noteInfo by lazy { args.findNoteInfo }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +25,14 @@ class SearchNoteDetailFragment : BaseFragment<FragmentSearchNoteDetailBinding>(F
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            btnBarcodeScan.setOnClickListener {
+                val action = SearchNoteDetailFragmentDirections
+                    .actionFragmentSearchNoteDetailToFragmentBarcodeScanner(noteInfo)
+                findNavController().navigate(action)
+            }
+        }
 
     }
 }
