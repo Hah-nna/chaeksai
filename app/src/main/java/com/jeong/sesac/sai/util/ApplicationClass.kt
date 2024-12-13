5d3fe92943fc5a7169134e393a969c116d53d9d7
Jiyeong <jeongjiyeong486@gmail.com>
2024-12-13 19:54:41 +0900

    Fix:
    - 사용하지 않는 코드가 주석처리되어 있어 삭제하였습니다
    
    by Jiyeong

--- app/src/main/java/com/jeong/sesac/sai/util/ApplicationClass.kt
+++ app/src/main/java/com/jeong/sesac/sai/util/ApplicationClass.kt
@@ -1,8 +1,6 @@
 package com.jeong.sesac.sai.util
 
 import android.app.Application
-import android.util.Log
-import com.kakao.sdk.common.util.Utility
 import com.kakao.vectormap.KakaoMapSdk
 
 class ApplicationClass : Application() {
@@ -10,8 +8,6 @@ class ApplicationClass : Application() {
     override fun onCreate() {
         super.onCreate()
         applicationClass = this
-        //var hashKey = Utility.getKeyHash(this)
-        //Log.e("TAG", hashKey)
         KakaoMapSdk.init(this, "4da58aee4bca744cc8b3764bdb74b99b")
 
     }
