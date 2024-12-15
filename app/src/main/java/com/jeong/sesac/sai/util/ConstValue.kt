eab00bf9ac9a50820d57457c88163527206dd49b
Hah-nna <hj08hn@gmail.com>
2024-12-15 20:53:58 +0900

    feat : toolbar title 상수값 파일 생성 - 툴바 타이틀을 상수로 뺐습니다
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/util/ConstValue.kt
@@ -0,0 +1,17 @@
+package com.jeong.sesac.sai.util
+
+/** toolbar title values*/
+const val RECENTLY_FOUND_NOTES_TOOLBAR_TITLE = "방금 찾은 쪽지"
+const val WEEKLY_NOTES_TOOLBAR_TITLE = "금주의 새로운 쪽지"
+const val SEARCH_NOTES_TOOLBAR_TITLE = "쪽지 찾기"
+const val GO_HOME_TOOLBAR_TITLE = "홈"
+const val BACK_TOOLBAR_TITLE = "이전으로"
+const val APP_SETTING_TOOLBAR_TITLE = "앱설정"
+const val RECEIVED_REVIEW_TOOLBAR_TITLE = "받은 리뷰"
+const val PRIVACY_MANAGEMENT_TOOLBAR_TITLE = "개인정보관리"
+const val FOLLOWER_TOOLBAR_TITLE = "팔로워"
+const val LOGIN_MANAGEMENT_TOOLBAR_TITLE = "로그인 관리"
+const val SETTING_MANAGEMENT_TOOLBAR_TITLE = "설정"
+const val MY_REGISTERED_NOTES_TOOLBAR_TITLE = "내가 등록한 쪽지"
+const val COMPLETED_FINDS_TOOLBAR_TITLE = "내가 찾은 쪽지"
+const val BOOKMARKED_NOTES_TOOLBAR_TITLE = "내가 찜한 쪽지"
