855c51518be13b0e035a829a692c8b728820c06d
Hah-nna <hj08hn@gmail.com>
2024-12-17 18:09:56 +0900

    feat : kakao map key 관련 설정
    - local.properties 안에 있는 내용을 읽어오는 함수를 만들고, BuildConfig 클래스 안에 추가될 수 있게 설정을 합니다(29, 30번째 라인)
    - buildFeatures를 true로 설정합니다
    
    @Hah_nna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,18 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safe.args)
     id("kotlin-parcelize")
+}
+
+// local.properties 안의 내용 읽기
+val localProperties: Properties by lazy {
+    Properties().apply {
+        rootProject.file("local.properties")
+            .takeIf { it.exists() }?.inputStream()?.use { load(it) }
+    }
 }
 
 android {
@@ -15,9 +25,10 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        
+       // kakao map 키 가져옴. manifest 파일에서도 사용할 수 있게 함
+        buildConfigField("String", "KAKAO_MAP_KEY", "\"${localProperties.getProperty("KAKAO_MAP_KEY", "")}\"")
+        manifestPlaceholders["KAKAO_MAP_MANIFEST_APP_KEY"] = localProperties.getProperty("KAKAO_MAP_MANAFEST_APP_KEY", "")
     }
 
     buildTypes {
@@ -29,6 +40,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -41,6 +53,14 @@ android {
         enable = true
     }
 
+    /** buildConfig.java 파일 생성을 ㅇㅋㅇㅋ함
+     * buildConfig.java 파일 속 BuildConfig 클래스 안에
+     * defaultConfig와 30번째 라인(buildConfig로 설정한 내용)이 저장됨
+     * 그리고 이 값들을 다른 곳에서도 val apiKey = BuildConfig.KAKAO_MAP_KEY <- 이런식으로 사용가능함
+     */
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -58,7 +78,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.corbind)
     implementation(libs.ru.ldralighieri.corbind.material)
     implementation("com.kakao.maps.open:android:2.9.5")
     implementation("com.kakao.sdk:v2-all:2.20.6")
