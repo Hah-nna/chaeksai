3b62b8b1ea4edb13abffc96c84cb97d37a4d45da
Hah-nna <hj08hn@gmail.com>
2024-12-17 18:41:16 +0900

    fix : constant values
    - 툴바타이틀을 상수로 분리하는 대신에 values-string에 다시 작성해서 R.string~형식으로 가져오게 수정했습니다(문자열값을 굳이 상수로 뺄 이유가 없는 거 같아서요)
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/util/ConstValue.kt
+++ app/src/main/java/com/jeong/sesac/sai/util/ConstValue.kt
@@ -1,37 +1,5 @@
 package com.jeong.sesac.sai.util
 
-/** toolbar title values */
-const val RECENTLY_FOUND_NOTES_TOOLBAR_TITLE = "방금 찾은 쪽지"
-const val WEEKLY_NOTES_TOOLBAR_TITLE = "금주의 새로운 쪽지"
-const val SEARCH_NOTES_TOOLBAR_TITLE = "쪽지 찾기"
-const val GO_HOME_TOOLBAR_TITLE = "홈"
-const val BACK_TOOLBAR_TITLE = "이전으로"
-const val APP_SETTING_TOOLBAR_TITLE = "앱설정"
-const val RECEIVED_REVIEW_TOOLBAR_TITLE = "받은 리뷰"
-const val PRIVACY_MANAGEMENT_TOOLBAR_TITLE = "개인정보관리"
-const val FOLLOWER_TOOLBAR_TITLE = "팔로워"
-const val LOGIN_MANAGEMENT_TOOLBAR_TITLE = "로그인 관리"
-const val SETTING_MANAGEMENT_TOOLBAR_TITLE = "설정"
-const val MY_REGISTERED_NOTES_TOOLBAR_TITLE = "내가 등록한 쪽지"
-const val COMPLETED_FINDS_TOOLBAR_TITLE = "내가 찾은 쪽지"
-const val BOOKMARKED_NOTES_TOOLBAR_TITLE = "내가 찜한 쪽지"
-const val WRITE_REVIEW_TOOLBAR_TITLE = "리뷰 작성"
-
-/** Dialog titles and messages */
-const val DIALOG_TITLE_DELETE_CASH = "캐시 데이터 삭제"
-const val DIALOG_MESSAGE_DELETE_CASH = "캐시 데이터를 삭제하시겠습니까?"
-
-/** Dialog button texts */
-const val DIALOG_BUTTON_TEXT_YES = "예"
-const val DIALOG_BUTTON_TEXT_NO = "아니오"
-
-/** Toast messages */
-const val TOAST_MESSAGE_CASH_DELETED = "캐시 데이터가 삭제되었습니다."
-
-/** Button tags */
-const val BUTTON_TAG_DEACTIVATE = "deactivate"
-const val BUTTON_TAG_ACTIVATE = "activate"
-
 /**Library name */
 const val SESAC_LIBRARY = "새싹 도서관"
 
