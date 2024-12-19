6333b8b86657f41df9b89f397107c467bfe16a16
Hah-nna <hj08hn@gmail.com>
2024-12-20 05:42:59 +0900

    feat : add WeeklyNotesInterface
    - WeeklyNotesInterface를 선언합니다
    - WeeklyNotesRepository에서 구현할 예정입니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/repository/WeeklyNotesRepository.kt
@@ -0,0 +1,9 @@
+package com.jeong.sesac.sai.repository
+
+import com.jeong.sesac.sai.repository.interfaces.WeeklyNotesInterface
+
+class WeeklyNotesRepository : WeeklyNotesInterface {
+    override fun getWeeklyNotes() {
+        TODO("등록은 되있고 아직 찾기 상태가 아닌(noteUserInteraction에 찾은 상태로 등록된 게 아닌 쪽지)")
+    }
+}
