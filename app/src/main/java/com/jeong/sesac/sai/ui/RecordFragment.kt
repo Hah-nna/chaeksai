ac69bfba4f27015a18539dd5c8ad305c7328e06f
Hah-nna <hj08hn@gmail.com>
2024-12-26 17:14:19 +0900

    fix : RecordFragment
    - 뷰페이저 어댑터를 연결합니다
    - 뷰페이저와 탭레이아웃을 연결합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/ui/RecordFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/RecordFragment.kt
@@ -1,63 +1,34 @@
+package com.jeong.sesac.sai.ui
+
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.lifecycleScope
-import androidx.viewpager2.widget.ViewPager2
-import com.jeong.sesac.sai.R
-import com.jeong.sesac.sai.databinding.FragmentRecordBinding
-import com.jeong.sesac.sai.ui.adapters.RecordPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.jeong.sesac.sai.databinding.FragmentTabLayoutBinding
+import com.jeong.sesac.sai.recycler.Record.RecordPagerAdapter
 import com.jeong.sesac.sai.util.BaseFragment
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.launch
-import ru.ldralighieri.corbind.view.clicks
 
-class RecordFragment : BaseFragment<FragmentRecordBinding>(FragmentRecordBinding::inflate) {
+class RecordFragment : BaseFragment<FragmentTabLayoutBinding>(FragmentTabLayoutBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding) {
-            setupViewPager()
-            setupButtonGroup()
-        }
-    }
+        /**
+         * ViewPager2에 RecordPagerAdapter 연결
+         */
+        binding.viewPager.adapter = RecordPagerAdapter(this@RecordFragment)
 
-    private fun FragmentRecordBinding.setupViewPager() {
-        viewPager.adapter = RecordPagerAdapter(this@RecordFragment)
-    }
+        /**
+         * TabLayout과 ViewPager2를 TabLayoutMediator로 연결
+         * */
 
-    private fun FragmentRecordBinding.setupButtonGroup() {
-        val buttons = listOf(buttonRegisteredNotes, buttonCompletedFinds, buttonBookmarkedNotes)
-
-        // Corbind를 사용하여 버튼 클릭 이벤트 처리
-        val buttonClicks = buttons.mapIndexed { index, button ->
-
-            // 클릭 시 해당 버튼의 index 반환
-            button.clicks().map { index }
-
-            // 여러 클릭 이벤트 Flow 병합
-        }.merge()
-
-        lifecycleScope.launch {
-            buttonClicks.collect { index ->
-
-                // 클릭된 버튼의 index에 따라 ViewPager 페이지 변경
-                viewPager.currentItem = index
+        TabLayoutMediator(binding.buttonGroup, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "등록한쪽지"
+                1 -> "찾은쪽지"
+                else -> "찜한쪽지"
             }
-        }
-
-        // ViewPager 페이지 변경에 따른 버튼 상태 업데이트
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                buttons.forEachIndexed { index, button ->
-                    button.isSelected = index == position
-                    button.setBackgroundColor(
-                        if (index == position) requireContext().getColor(R.color.primary)
-                        else requireContext().getColor(R.color.black)
-                    )
-                }
-            }
-        })
+        }.attach()
     }
+
+
 }
