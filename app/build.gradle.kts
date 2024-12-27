a55c6e0f9b7a4ca8b0927c356bd861b0d5879a26
Hah-nna <hj08hn@gmail.com>
2024-12-27 21:12:03 +0900

    chore : build.gradle
    - app level build.gradle에 domainModule을 추가합니다
    
    @Hah_nna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.ru.ldralighieri.corbind)
+    implementation(project(":domainModule"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
