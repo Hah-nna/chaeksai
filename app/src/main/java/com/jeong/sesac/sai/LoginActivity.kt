3cbdd21ab824321ed4559e9b3502afee3daed4ad
Hah-nna <hj08hn@gmail.com>
2025-01-02 01:59:04 +0900

    design : add editTextView background change
    - 포커스 여부에 따라 editTextView background를 다르게 설정합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
+++ app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
@@ -3,34 +3,32 @@ package com.jeong.sesac.sai
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.jeong.sesac.datamodule.datasource.FireBaseDataSourceImpl
 import com.jeong.sesac.datamodule.repository.LoginRepositoryImpl
 import com.jeong.sesac.domain.model.UserInfo
 import com.jeong.sesac.sai.databinding.ActivityLoginBinding
-import com.jeong.sesac.sai.databinding.FragmentLoginByNicknameBinding
-import com.jeong.sesac.sai.ui.login.LoginFragment
 import com.jeong.sesac.sai.util.AppPreferenceManager
 import com.jeong.sesac.sai.util.throttleFirst
 import com.jeong.sesac.sai.util.throttleTime
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import ru.ldralighieri.corbind.view.clicks
+import ru.ldralighieri.corbind.widget.textChanges
 import java.time.LocalDateTime
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val loginRepo = LoginRepositoryImpl(FireBaseDataSourceImpl())
-    private val preference by lazy { AppPreferenceManager.getInstance(this)}
+    private val preference by lazy { AppPreferenceManager.getInstance(this) }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,54 +38,79 @@ class LoginActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
+        /**
+         * editText 포커스 여부에 따라 색 변경
+         * */
+        setETBackground()
+
+
+        binding.etvNickname.textChanges()
+            .debounce(500L)
+            .onEach { char ->
+                // 0.5초마다 debounce하는데요
+                // 1. 특수문자, 숫자로만 되어있는지(숫자로 안 되어있어야함), 2글자 이상인지 확인
+                // 2. 1의 경우가 다 참이라면 etvNickname.getText()를 파베에 쿼리로 조회해서
+                // 중복되는게 있는지 없는지 확인
+                // --- 여기까지 앱 사용하러가기는 Enabled = false여야함
+                //
+            }
+            .launchIn(lifecycleScope)
+
+
+        /*
+        *
+        * 닉네임 받아서 유저생성 및 preference에 저장
+        * */
         with(binding) {
             btnSetUser.clicks().throttleFirst(throttleTime).onEach {
                 val nickname = etvNickname.text.toString()
-                if(nickname.isNotEmpty()) {
-                    try {
-                        lifecycleScope.launch {
-                        val result = loginRepo.setUser(UserInfo(
-                                "",
-                                nickname,
-                                "",
-                                LocalDateTime.now().toString()
-                            ))
-
-                            if(result) {
-                                preference.nickName = nickname
-                                Intent(this@LoginActivity, MainActivity::class.java).run {
-                                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                                    startActivity(this)
-                                }
-                                    finish()
-
-                            }
-
-                        }
-
-                    } catch (e : Exception) {
-                        Log.d("error", e.message.toString())
-                            Toast.makeText(this@LoginActivity, e.message, LENGTH_SHORT).show()
-                    }
-                }
+                setUser(nickname)
             }.launchIn(lifecycleScope)
         }
     }
 
-    override fun onResume() {
-        super.onResume()
 
+    private fun setETBackground() {
+        with(binding.etvNickname) {
+            setOnFocusChangeListener { _, hasFocus ->
+                backgroundTintList = when (hasFocus) {
+                    true ->
+                        ContextCompat.getColorStateList(applicationContext, R.color.tertiary)
 
+                    else -> ContextCompat.getColorStateList(applicationContext, R.color.black)
+                }
+            }
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
-        Log.d("LoginActivity onStop", "onStop")
-    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setUser(nickname: String) {
+        if (nickname.isNotEmpty()) {
+            try {
+                lifecycleScope.launch {
+                    val result = loginRepo.setUser(
+                        UserInfo(
+                            "",
+                            nickname,
+                            "",
+                            LocalDateTime.now().toString()
+                        )
+                    )
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("LoginActivity onDestroy", "onDestroy")
-    }
+                    if (result) {
+                        preference.nickName = nickname
+                        Intent(this@LoginActivity, MainActivity::class.java).run {
+                            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(this)
+                        }
+                        finish()
+                    }
+                }
 
+            } catch (e: Exception) {
+                Log.d("error", e.message.toString())
+                Toast.makeText(this@LoginActivity, e.message, LENGTH_SHORT).show()
+            }
+        }
+    }
 }
