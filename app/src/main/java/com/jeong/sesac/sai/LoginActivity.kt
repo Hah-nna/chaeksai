b8fbd8152217e2a4c5a0ae3d23ea46e996df3331
Hah-nna <hj08hn@gmail.com>
2025-01-02 18:14:00 +0900

    feat : 유효성 검사 및 닉네임 중복검사에 따른 ui 설정
    - 닉네임 유효성 검사와 닉네임 중복검사에 대한 ui를 설정합니다
    - 일차적으로 유효성검사를 통과하지 못하면 관련 메세지를 보여줍니다
    - 유효성 검사를 통과하고 이차적으로 유효성검사를 다시하고 통과했다면 닉네임 중복검사를 시행합니다
    - 관련 내용을 함수로 분리합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
+++ app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
@@ -43,22 +43,12 @@ class LoginActivity : AppCompatActivity() {
          * */
         setETBackground()
 
-
-        binding.etvNickname.textChanges()
-            .debounce(500L)
-            .onEach { char ->
-                // 0.5초마다 debounce하는데요
-                // 1. 특수문자, 숫자로만 되어있는지(숫자로 안 되어있어야함), 2글자 이상인지 확인
-                // 2. 1의 경우가 다 참이라면 etvNickname.getText()를 파베에 쿼리로 조회해서
-                // 중복되는게 있는지 없는지 확인
-                // --- 여기까지 앱 사용하러가기는 Enabled = false여야함
-                //
-            }
-            .launchIn(lifecycleScope)
-
+        /**
+         * 유효성 검사 및 닉네임 중복검사
+         * */
+        checkValidNickname()
 
         /*
-        *
         * 닉네임 받아서 유저생성 및 preference에 저장
         * */
         with(binding) {
@@ -69,7 +59,109 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 닉네임 유효성 검사 및 중복검사 함수
+     * 닉네임 유효범위 : 숫자, 한글, 영어 조합으로 총 8글자까지만 가능
+     * 닉네임 비유효범위 : 숫자로만 이루어져있을 때, 특수문자, 공백, 2글자 미만
+     *
+     * */
+    private fun checkValidNickname() {
+        binding.etvNickname.textChanges()
+            // 처음 emit되는 빈값 drop
+            .dropInitialValue()
+            .onEach { char ->
+                // 로그를 찍어보니 하나하나 emit되는 char 값이 계속 더해짐 (ex: n -> ni -> nic -> nick 이런식으로)
+                val inputText = char.toString()
+                // 유효성 검사 함
+                val isValidText = isValidNickname(inputText)
+                when {
+                    // 유효성 검사 통과 못했을 시
+                    !isValidText -> {
+                        binding.tvNicknameCheck.setHint("특수문자와 공백은 사용할 수 없습니다")
+                        binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.red))
+                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.red)
+                        return@onEach
+                    }
+                    // 입력된 텍스트가 8글자 이상(유효범위 벗어남)
+                    inputText.length > 8 -> {
+                        binding.tvNicknameCheck.setHint("8글자까지만 가능합니다")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.inValid
+                            )
+                        )
+                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.inValid)
+                        return@onEach
+                    }
+                    // 입력된 텍스트가 2글자 미만(유효범위 벗어남)
+                    inputText.length < 2 -> {
+                        binding.tvNicknameCheck.setHint("2글자 이상 8글자 이하만 가능합니다")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.inValid
+                            )
+                        )
+                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.inValid)
+                        return@onEach
+                    }
+                    // 통과한 경우
+                    // 메세지는 딱히 없고 색깔만 변경
+                    else -> {
+                        binding.tvNicknameCheck.setHint("")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.valid
+                            )
+                        )
+                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.valid)
+                    }
+                }
+            }
+            // 위의 text가 더 생성이 안 되면(이벤트 x) 마지막 이벤트 0.5후에 아래를 실행
+            .debounce(500L)
+            .onEach { nickName ->
+                val nickname = nickName.toString()
+                // 닉네임 전체에 대해서 다시 유효성 검사 시행
+                if (isValidNickname(nickname)) {
+                    // 리턴값이 참이면 중복검사
+                    val result = loginRepo.checkDuplicateNickname(nickname)
+                    when (result) {
+                        // 값이 참이라면 중복된 값이 있다는 뜻이므로 존재하는 닉네임임
+                        true -> {
+                            binding.tvNicknameCheck.setHint("이미 존재하는 닉네임입니다")
+                            binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.inValid))
+                        }
+                        // 아니라면 닉네임이 없는거니까 사용할 수 있는 닉네임임
+                        else -> {
+                            binding.tvNicknameCheck.setHint("사용할 수 있는 닉네임입니다")
+                            binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.valid))
+                            binding.btnSetUser.isEnabled = true
+                        }
+                    }
+                }
 
+            }
+            .launchIn(lifecycleScope)
+    }
+
+
+    private fun isValidNickname(nickname: String): Boolean {
+        val onlyNumber = "[0-9]".toRegex()
+        val validCharsRegex = "^[a-zA-Z가-힣0-9]+$".toRegex()
+
+        return when {
+            nickname.isEmpty() || nickname.length < 2 ||nickname.length > 8 -> false
+            !validCharsRegex.matches(nickname) -> false
+            onlyNumber.matches(nickname) -> false
+            else -> true
+        }
+    }
+
+
+    // 포커스됨에 따라서 edit text view 색깔 변경
     private fun setETBackground() {
         with(binding.etvNickname) {
             setOnFocusChangeListener { _, hasFocus ->
@@ -83,6 +175,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    // 유저 생성
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setUser(nickname: String) {
         if (nickname.isNotEmpty()) {
