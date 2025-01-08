f8d35ad04bd596509026f84030fcdb52f54cf0e1
Hah-nna <hj08hn@gmail.com>
2025-01-08 20:04:35 +0900

    fix : 순서 변경
    - 순서를 변경합니다
    
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/recentlyFoundNote/DiffUtilRecentlyFoundNote.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/recentlyFoundNote/DiffUtilRecentlyFoundNote.kt
@@ -4,11 +4,12 @@ import androidx.recyclerview.widget.DiffUtil
 import com.jeong.sesac.sai.util.WeeklyNotesInfo
 
 class DiffUtilRecentlyFoundNote : DiffUtil.ItemCallback<WeeklyNotesInfo>() {
+    override fun areItemsTheSame(oldItem: WeeklyNotesInfo, newItem: WeeklyNotesInfo): Boolean {
+        return oldItem == newItem
+    }
+
     override fun areContentsTheSame(oldItem: WeeklyNotesInfo, newItem: WeeklyNotesInfo): Boolean {
         return oldItem.id == newItem.id
     }
 
-    override fun areItemsTheSame(oldItem: WeeklyNotesInfo, newItem: WeeklyNotesInfo): Boolean {
-        return oldItem == newItem
-    }
 }
