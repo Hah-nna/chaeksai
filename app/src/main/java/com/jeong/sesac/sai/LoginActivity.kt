611b78ea26758eed5e83b8d1bf3c20683d7503ac
Hah-nna <hj08hn@gmail.com>
2025-01-08 20:16:24 +0900

    fix : login duplicate check and isValid test - 아이디 유효성검사와 중복검사를 mvvm대로 나누어 각 역할을 분리함
    @Hah_nna

--- app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
+++ app/src/main/java/com/jeong/sesac/sai/LoginActivity.kt
@@ -4,35 +4,39 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
-import com.jeong.sesac.datamodule.datasource.FireBaseDataSourceImpl
-import com.jeong.sesac.datamodule.repository.LoginRepositoryImpl
-import com.jeong.sesac.domain.model.UserInfo
 import com.jeong.sesac.sai.databinding.ActivityLoginBinding
 import com.jeong.sesac.sai.util.AppPreferenceManager
 import com.jeong.sesac.sai.util.throttleFirst
 import com.jeong.sesac.sai.util.throttleTime
+import com.jeong.sesac.sai.viewmodel.LoginViewModel
+import com.jeong.sesac.sai.viewmodel.entity.UiState
+import com.jeong.sesac.sai.viewmodel.factory.appViewModelFactory
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import ru.ldralighieri.corbind.view.clicks
 import ru.ldralighieri.corbind.widget.textChanges
-import java.time.LocalDateTime
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private val loginRepo = LoginRepositoryImpl(FireBaseDataSourceImpl())
     private val preference by lazy { AppPreferenceManager.getInstance(this) }
+    private val viewModel: LoginViewModel by viewModels<LoginViewModel> {
+        appViewModelFactory
+    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("LoginActivity create", "create")
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater).also {
             setContentView(it.root)
@@ -47,6 +51,7 @@ class LoginActivity : AppCompatActivity() {
          * 유효성 검사 및 닉네임 중복검사
          * */
         checkValidNickname()
+
 
         /*
         * 닉네임 받아서 유저생성 및 preference에 저장
@@ -65,7 +70,9 @@ class LoginActivity : AppCompatActivity() {
      * 닉네임 비유효범위 : 숫자로만 이루어져있을 때, 특수문자, 공백, 2글자 미만
      *
      * */
+    @OptIn(FlowPreview::class)
     private fun checkValidNickname() {
+
         binding.etvNickname.textChanges()
             // 처음 emit되는 빈값 drop
             .dropInitialValue()
@@ -75,11 +82,29 @@ class LoginActivity : AppCompatActivity() {
                 // 유효성 검사 함
                 val isValidText = isValidNickname(inputText)
                 when {
+                    inputText.length < 2 -> {
+                        binding.tvNicknameCheck.setHint("2글자 이상 8글자 이하만 가능합니다")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.inValid
+                            )
+                        )
+                        binding.etvNickname.backgroundTintList =
+                            ContextCompat.getColorStateList(applicationContext, R.color.inValid)
+                        return@onEach
+                    }
                     // 유효성 검사 통과 못했을 시
                     !isValidText -> {
                         binding.tvNicknameCheck.setHint("특수문자와 공백은 사용할 수 없습니다")
-                        binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.red))
-                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.red)
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this,
+                                R.color.red
+                            )
+                        )
+                        binding.etvNickname.backgroundTintList =
+                            ContextCompat.getColorStateList(applicationContext, R.color.red)
                         return@onEach
                     }
                     // 입력된 텍스트가 8글자 이상(유효범위 벗어남)
@@ -91,21 +116,12 @@ class LoginActivity : AppCompatActivity() {
                                 R.color.inValid
                             )
                         )
-                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.inValid)
+                        binding.etvNickname.backgroundTintList =
+                            ContextCompat.getColorStateList(applicationContext, R.color.inValid)
                         return@onEach
                     }
                     // 입력된 텍스트가 2글자 미만(유효범위 벗어남)
-                    inputText.length < 2 -> {
-                        binding.tvNicknameCheck.setHint("2글자 이상 8글자 이하만 가능합니다")
-                        binding.tvNicknameCheck.setHintTextColor(
-                            ContextCompat.getColor(
-                                this,
-                                R.color.inValid
-                            )
-                        )
-                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.inValid)
-                        return@onEach
-                    }
+
                     // 통과한 경우
                     // 메세지는 딱히 없고 색깔만 변경
                     else -> {
@@ -116,7 +132,8 @@ class LoginActivity : AppCompatActivity() {
                                 R.color.valid
                             )
                         )
-                        binding.etvNickname.backgroundTintList = ContextCompat.getColorStateList(applicationContext, R.color.valid)
+                        binding.etvNickname.backgroundTintList =
+                            ContextCompat.getColorStateList(applicationContext, R.color.valid)
                     }
                 }
             }
@@ -127,24 +144,46 @@ class LoginActivity : AppCompatActivity() {
                 // 닉네임 전체에 대해서 다시 유효성 검사 시행
                 if (isValidNickname(nickname)) {
                     // 리턴값이 참이면 중복검사
-                    val result = loginRepo.checkDuplicateNickname(nickname)
-                    when (result) {
-                        // 값이 참이라면 중복된 값이 있다는 뜻이므로 존재하는 닉네임임
-                        true -> {
-                            binding.tvNicknameCheck.setHint("이미 존재하는 닉네임입니다")
-                            binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.inValid))
-                        }
-                        // 아니라면 닉네임이 없는거니까 사용할 수 있는 닉네임임
-                        else -> {
-                            binding.tvNicknameCheck.setHint("사용할 수 있는 닉네임입니다")
-                            binding.tvNicknameCheck.setHintTextColor(ContextCompat.getColor(this, R.color.valid))
-                            binding.btnSetUser.isEnabled = true
-                        }
-                    }
+                    viewModel.checkDuplicatedNickname(nickname)
                 }
 
             }
             .launchIn(lifecycleScope)
+
+
+        lifecycleScope.launch {
+            viewModel.duplicateState.collectLatest {
+                when (it) {
+                    is UiState.Loading -> {
+                        binding.progressBar.progressCircular.visibility = View.VISIBLE
+                    }
+
+                    is UiState.Success -> {
+                        binding.progressBar.progressCircular.visibility = View.GONE
+                        binding.tvNicknameCheck.setHint("사용할 수 있는 닉네임입니다")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this@LoginActivity,
+                                R.color.valid
+                            )
+                        )
+                        binding.btnSetUser.isEnabled = true
+                    }
+
+                    is UiState.Error -> {
+                        binding.progressBar.progressCircular.visibility = View.GONE
+                        binding.tvNicknameCheck.setHint("이미 존재하는 닉네임입니다")
+                        binding.tvNicknameCheck.setHintTextColor(
+                            ContextCompat.getColor(
+                                this@LoginActivity,
+                                R.color.inValid
+                            )
+                        )
+                        binding.btnSetUser.isEnabled = false
+                    }
+                }
+            }
+        }
     }
 
 
@@ -153,7 +192,7 @@ class LoginActivity : AppCompatActivity() {
         val validCharsRegex = "^[a-zA-Z가-힣0-9]+$".toRegex()
 
         return when {
-            nickname.isEmpty() || nickname.length < 2 ||nickname.length > 8 -> false
+            nickname.isEmpty() || nickname.length < 2 || nickname.length > 8 -> false
             !validCharsRegex.matches(nickname) -> false
             onlyNumber.matches(nickname) -> false
             else -> true
@@ -178,32 +217,30 @@ class LoginActivity : AppCompatActivity() {
     // 유저 생성
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setUser(nickname: String) {
-        if (nickname.isNotEmpty()) {
-            try {
-                lifecycleScope.launch {
-                    val result = loginRepo.setUser(
-                        UserInfo(
-                            "",
-                            nickname,
-                            "",
-                            LocalDateTime.now().toString()
-                        )
-                    )
-
-                    if (result) {
+        viewModel.serUserInfo(nickname)
+        lifecycleScope.launch {
+            viewModel.userCreateState.collectLatest {
+                when(it) {
+                    is UiState.Loading -> {
+                        binding.progressBar.progressCircular.visibility = View.VISIBLE
+                    }
+                    is UiState.Success -> {
                         preference.nickName = nickname
                         Intent(this@LoginActivity, MainActivity::class.java).run {
                             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            Log.d("intent to mainActivity", "INTENT")
                             startActivity(this)
                         }
                         finish()
                     }
+                    is UiState.Error -> {
+                        Toast.makeText(this@LoginActivity, "에러 발생", LENGTH_SHORT).show()
+                    }
                 }
-
-            } catch (e: Exception) {
-                Log.d("error", e.message.toString())
-                Toast.makeText(this@LoginActivity, e.message, LENGTH_SHORT).show()
             }
         }
+
     }
+
+
 }
