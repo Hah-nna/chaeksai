e94a0406589f9b2e79084b8153448f6c331695e9
Hah-nna <hj08hn@gmail.com>
2025-01-08 20:03:05 +0900

    feat : kakaomap recycler
    - kakaoMap에 보여줄 리사이클러뷰를 생성합니다
    
    @Hah_nna

---
+++ app/src/main/java/com/jeong/sesac/sai/recycler/map/MapViewHolder.kt
@@ -0,0 +1,7 @@
+package com.jeong.sesac.sai.recycler.map
+
+import androidx.recyclerview.widget.RecyclerView
+import com.jeong.sesac.sai.databinding.ItemLibraryInfoBinding
+
+class MapViewHolder(val binding : ItemLibraryInfoBinding) : RecyclerView.ViewHolder(binding.root) {
+}
