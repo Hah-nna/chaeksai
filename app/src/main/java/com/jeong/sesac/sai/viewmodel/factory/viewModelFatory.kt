ed74d526db0abb2bbecd10bc197d47d475e7f641
Hah-nna <hj08hn@gmail.com>
2025-01-22 05:40:21 +0900

    rename : datamodule -> data
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
+++ app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
@@ -2,18 +2,16 @@ package com.jeong.sesac.sai.viewmodel.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.jeong.sesac.datamodule.api.manager.KakaoMapManager
-import com.jeong.sesac.datamodule.api.manager.OpenAPIManager
-import com.jeong.sesac.datamodule.api.service.KakaoMapService
-import com.jeong.sesac.datamodule.datasource.FireBaseDataSourceImpl
-import com.jeong.sesac.datamodule.datasource.KakaoMapDataSourceImpl
-import com.jeong.sesac.datamodule.datasource.OpenAPIDataSourceImpl
-import com.jeong.sesac.datamodule.repository.KakaoMapRepositoryImpl
-import com.jeong.sesac.datamodule.repository.LoginRepositoryImpl
-import com.jeong.sesac.datamodule.repository.OpenApiRepositoryImpl
+import com.jeong.sesac.data.api.manager.KakaoMapManager
+import com.jeong.sesac.data.api.manager.OpenAPIManager
+import com.jeong.sesac.data.datasource.FireBaseDataSourceImpl
+import com.jeong.sesac.data.datasource.KakaoMapDataSourceImpl
+import com.jeong.sesac.data.datasource.OpenAPIDataSourceImpl
+import com.jeong.sesac.data.repository.KakaoMapRepositoryImpl
+import com.jeong.sesac.data.repository.LoginRepositoryImpl
+import com.jeong.sesac.data.repository.OpenApiRepositoryImpl
 import com.jeong.sesac.sai.viewmodel.KakaoMapViewModel
 import com.jeong.sesac.sai.viewmodel.LoginViewModel
-import com.jeong.sesac.sai.viewmodel.RegisterNoteViewModel
 
 @Suppress("UNCHECKED_CAST")
 val appViewModelFactory = object : ViewModelProvider.Factory {
@@ -25,10 +23,6 @@ val appViewModelFactory = object : ViewModelProvider.Factory {
                 isAssignableFrom(KakaoMapViewModel::class.java) -> {
                     val kakaoService = KakaoMapManager.getInstance()
                     KakaoMapViewModel(KakaoMapRepositoryImpl(KakaoMapDataSourceImpl(kakaoService)))
-                }
-                isAssignableFrom(RegisterNoteViewModel::class.java) -> {
-                    val OpenApiService = OpenAPIManager.getInstance()
-                    RegisterNoteViewModel(OpenApiRepositoryImpl(OpenAPIDataSourceImpl(OpenApiService)))
                 }
                 else ->
                     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
