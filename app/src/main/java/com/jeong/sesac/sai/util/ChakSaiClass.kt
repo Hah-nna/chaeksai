563792eda9c83280d407e5038b9d48d23ddc4596
Hah-nna <hj08hn@gmail.com>
2025-01-22 05:42:23 +0900

    fix : 하드코딩된 값 변경
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/util/ChakSaiClass.kt
+++ app/src/main/java/com/jeong/sesac/sai/util/ChakSaiClass.kt
@@ -1,20 +1,21 @@
 package com.jeong.sesac.sai.util
 
 import android.app.Application
+import com.jeong.sesac.sai.BuildConfig
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.vectormap.KakaoMapSdk
 
-class ApplicationClass : Application() {
+class ChakSaiClass : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appInstance = this
-        KakaoMapSdk.init(this, "4da58aee4bca744cc8b3764bdb74b99b")
-        KakaoSdk.init(this, "4da58aee4bca744cc8b3764bdb74b99b" )
+        KakaoMapSdk.init(this, BuildConfig.KAKAO_API_KEY)
+        KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY )
 
     }
     companion object{
-        private lateinit var appInstance : ApplicationClass
+        private lateinit var appInstance : ChakSaiClass
         fun getContext() = appInstance
 
     }
