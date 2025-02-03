ac695bf67c0b34b0909b4b68f76fa8dd8e8060bf
Hah-nna <hj08hn@gmail.com>
2025-02-03 10:35:02 +0900

    fix: note list - 쪽지리스트를 타입에 따라서 다르게 받아오도록 수정합니다
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/ui/record/RecordListFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/record/RecordListFragment.kt
@@ -1,68 +1,99 @@
 package com.jeong.sesac.sai.ui.record
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import com.jeong.sesac.sai.R
+import com.jeong.sesac.domain.model.NoteFilterType
 import com.jeong.sesac.sai.databinding.ItemTabRecyclerBinding
+import com.jeong.sesac.sai.model.UiState
 import com.jeong.sesac.sai.recycler.GridDecoration
-import com.jeong.sesac.sai.recycler.Record.RecordRecyclerAdapter
-import com.jeong.sesac.sai.ui.RecordFragmentDirections
+import com.jeong.sesac.sai.recycler.record.RecordRecyclerAdapter
+import com.jeong.sesac.sai.util.AppPreferenceManager
 import com.jeong.sesac.sai.util.BaseFragment
-import com.jeong.sesac.sai.util.WeeklyNoteMockData
+import com.jeong.sesac.sai.viewmodel.NoteListViewModel
+import com.jeong.sesac.sai.viewmodel.factory.appViewModelFactory
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class RecordListFragment : BaseFragment<ItemTabRecyclerBinding>(ItemTabRecyclerBinding::inflate) {
 
-
-    private lateinit var recordAdapter : RecordRecyclerAdapter
+    private lateinit var recordAdapter: RecordRecyclerAdapter
+    private lateinit var preference: AppPreferenceManager
+    private val viewModel: NoteListViewModel by viewModels <NoteListViewModel> { appViewModelFactory }
 
     companion object {
-        fun getInstance(position : Int) =
+        fun getInstance(position: Int) =
             RecordListFragment().apply {
                 arguments = Bundle().apply {
                     putInt("position", position)
                 }
-        }
+            }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        preference = AppPreferenceManager.getInstance(requireContext())
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    /**
-     * recordAdapter의 {...} 은 callBack을 구현한 것임
-     * RecordRecyclerAdapter에서 아이템들이 클릭될 때 arguement에 있는 position이라는 키를 가진 value의 값을 보고
-     * 각 아이템을 누르면 어느 프래그먼트로 이동해야하는지를 설정함
-     * */
-        recordAdapter = RecordRecyclerAdapter { weeklyNotesInfo ->
-        val action = when (arguments?.getInt("position") ?: 0) {
-            0 -> {
-                RecordFragmentDirections.actionFragmentRecordToFragmentMyRegisteredNotes(
-                    "등록한쪽지", "이건쪽지", R.drawable.example_note
+        /**
+         * recordAdapter의 {...} 은 callBack을 구현한 것임
+         * RecordRecyclerAdapter에서 아이템들이 클릭될 때 arguement에 있는 position이라는 키를 가진 value의 값을 보고
+         * 각 아이템을 누르면 어느 프래그먼트로 이동해야하는지를 설정함
+         * */
+        recordAdapter = RecordRecyclerAdapter { note ->
+            val action =
+                RecordFragmentDirections.actionFragmentRecordToFragmentLibraryNoteDetail(
+                    note.id
                 )
-            }
-
-            1 -> {
-                RecordFragmentDirections.actionFragmentRecordToFragmentCompletedFinds(
-                    "찾은쪽지", "이건쪽지", R.drawable.note03
-                )
-            }
-
-            else -> {
-                RecordFragmentDirections.actionFragmentRecordToFragmentBookmarkedNotes(
-                    "북마크쪽지", "이건쪽지", R.drawable.note02
-                )
-            }
-
+            findNavController().navigate(action)
         }
-        findNavController().navigate(action)
-    }
 
         with(binding.rvContainer) {
             layoutManager = GridLayoutManager(requireContext(), 2)
             addItemDecoration(GridDecoration(requireContext()))
-            adapter = recordAdapter
+            adapter = this@RecordListFragment.recordAdapter
         }
-        recordAdapter.submitList(WeeklyNoteMockData.notesList)
+
+
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                val filteredList = when (arguments?.getInt("position") ?: 0) {
+                    0 -> NoteFilterType.MyNotes
+                    else -> NoteFilterType.MyLikedNotes
+                }
+                viewModel.getNoteList(filteredList, preference.nickName)
+
+                viewModel.noteListState.collectLatest { state ->
+                    when (state) {
+                        is UiState.Loading -> binding.progress.progressCircular.isVisible = true
+                        is UiState.Success -> {
+                            binding.progress.progressCircular.isVisible = false
+                            recordAdapter.submitList(state.data)
+                        }
+
+                        is UiState.Error -> {
+                            binding.progress.progressCircular.isVisible = false
+                            Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+            }
+        }
+
     }
 }
