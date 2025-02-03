37ebb7edb296653b790cc85c6dd57549f371ea3d
Hah-nna <hj08hn@gmail.com>
2025-02-03 11:27:06 +0900

    feat: map 검색 결과 관련 프래그먼트 생성
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/ui/libraryMap/noteList/SearchNoteDetailFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/libraryMap/noteList/SearchNoteDetailFragment.kt
@@ -1,11 +1,9 @@
-package com.jeong.sesac.sai.ui.searchRegister.search
+package com.jeong.sesac.sai.ui.libraryMap.noteList
 
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
-import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.navArgs
 import androidx.navigation.fragment.findNavController
 import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.FragmentSearchNoteDetailBinding
@@ -18,8 +16,8 @@ import ru.ldralighieri.corbind.view.clicks
 
 class SearchNoteDetailFragment : BaseFragment<FragmentSearchNoteDetailBinding>(FragmentSearchNoteDetailBinding::inflate){
 
-    private val args : SearchNoteDetailFragmentArgs by navArgs()
-    private val noteInfo by lazy { args.findNoteInfo }
+//    private val args : SearchNoteDetailFragmentArgs by navArgs()
+//    private val noteInfo by lazy { args.findNoteInfo }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,9 +26,9 @@ class SearchNoteDetailFragment : BaseFragment<FragmentSearchNoteDetailBinding>(F
         with(binding) {
             toolbar.toolbarView.setTitle(R.string.SEARCH_NOTES_TOOLBAR_TITLE)
             btnBarcodeScan.clicks().throttleFirst(throttleTime).onEach {
-                val action = SearchNoteDetailFragmentDirections
-                    .actionFragmentSearchNoteDetailToFragmentBarcodeScanner(noteInfo)
-                findNavController().navigate(action)
+//                val action = SearchNoteDetailFragmentDirections
+//                    .actionFragmentSearchNoteDetailToFragmentBarcodeScanner(noteInfo)
+//                findNavController().navigate(action)
             }.launchIn(viewLifecycleOwner.lifecycleScope)
 
             toolbar.toolbarView.clicks().throttleFirst(throttleTime).onEach {
