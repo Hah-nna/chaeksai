015b7bf3d7dc42fbb1723e28d48fd2998fa0af0d
Hah-nna <hj08hn@gmail.com>
2025-02-03 10:51:05 +0900

    feat: note color background
    @by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/sai/util/NoteBackground.kt
@@ -0,0 +1,17 @@
+package com.jeong.sesac.sai.util
+
+import android.graphics.Color
+import com.jeong.sesac.sai.R
+
+class NoteBackgroundColor() {
+    companion object {
+        val colors = listOf(
+            R.color.note_bg_1,
+            R.color.note_bg_2,
+            R.color.note_bg_3,
+            R.color.note_bg_4,
+            R.color.note_bg_5,
+            R.color.note_bg_6
+            )
+    }
+}
