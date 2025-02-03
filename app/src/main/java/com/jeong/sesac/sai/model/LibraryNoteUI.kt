55269c77c9b3b94a8777335ef2aa1601a61e6808
Hah-nna <hj08hn@gmail.com>
2025-02-03 11:35:25 +0900

    feat: data model 추가
    @by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/sai/model/LibraryNoteUI.kt
@@ -0,0 +1,69 @@
+package com.jeong.sesac.sai.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class LikeUI(
+    val id: String,
+    val noteId: String,
+    val userId: String,
+    val isLiked: Boolean
+) : Parcelable
+
+@Parcelize
+data class LibraryNoteUI(
+    val id: String,
+    val userInfo: UserInfo,
+    val image: String,
+    val title: String,
+    val createdAt: String,
+    val libraryName: String,
+    val content: String,
+    val likes: Int,
+//    val comments: List<CommentUI>
+) : Parcelable
+
+@Parcelize
+data class UserInfo(
+    val id: String,
+    val profile: String,
+    val nickName: String
+): Parcelable
+
+@Parcelize
+data class UserUI(
+    val id: String,
+    val profile: String,
+    val nickname: String,
+    val createdAt: String,
+    val providerInfo: ProviderTypeUI = ProviderTypeUI.KAKAO
+) : Parcelable
+
+@Parcelize
+data class CommentUI(
+    val id: String,
+    val userInfo: UserInfo,
+    val noteId: String,
+    val content: String,
+    val createdAt: String
+) : Parcelable
+
+@Parcelize
+enum class ProviderTypeUI : Parcelable {
+    KAKAO,
+    NAVER,
+    GOOGLE
+}
+
+@Parcelize
+data class PlaceInfoUI(
+    val id: String,
+    val address: String,
+    val phone: String,
+    val place: String,
+    val placeUrl: String,
+    val x: String,
+    val y: String
+) : Parcelable
