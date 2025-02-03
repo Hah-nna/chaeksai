758e8d142aad8b7ef8b1d45b1773ef442c32ab03
Hah-nna <hj08hn@gmail.com>
2025-02-03 11:01:07 +0900

    feat: chatting 관련 프래그먼트 및 뷰홀더 생성
    @by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/sai/ui/chat/ChatMainFragment.kt
@@ -0,0 +1,7 @@
+package com.jeong.sesac.sai.ui.chat
+
+import com.jeong.sesac.sai.databinding.FragmentChatMainBinding
+import com.jeong.sesac.sai.util.BaseFragment
+
+class ChatMainFragment : BaseFragment<FragmentChatMainBinding>(FragmentChatMainBinding::inflate) {
+}
