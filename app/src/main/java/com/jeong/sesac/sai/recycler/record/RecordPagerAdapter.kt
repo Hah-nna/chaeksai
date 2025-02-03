72a56e9ea1f780f934d822936fce1de068bd9eca
Hah-nna <hj08hn@gmail.com>
2025-02-03 10:33:27 +0900

    fix: getItemCount 3 -> 2
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/record/RecordPagerAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/record/RecordPagerAdapter.kt
@@ -1,4 +1,4 @@
-package com.jeong.sesac.sai.recycler.Record
+package com.jeong.sesac.sai.recycler.record
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -7,7 +7,7 @@ import com.jeong.sesac.sai.ui.record.RecordListFragment
 class RecordPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     /** viewPager에서 보여줄 총 프래그먼트 수 */
-    override fun getItemCount() = 3
+    override fun getItemCount() = 2
 
     /**
      * 항목을 구성하는 프래그먼트 객체를 얻음
