b76155d4fbfd489501b27fe877a292a607fc7508
Hah-nna <hj08hn@gmail.com>
2025-02-03 10:14:47 +0900

    feat: coil - coil을 적용합니다
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/weeklyNote/WeeklyNoteAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/weeklyNote/WeeklyNoteAdapter.kt
@@ -3,10 +3,14 @@ package com.jeong.sesac.sai.recycler.weeklyNote
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import coil3.load
+import coil3.request.crossfade
+import coil3.size.Scale
+import com.jeong.sesac.feature.model.NoteWithUser
 import com.jeong.sesac.sai.databinding.ItemWeeklyNotesBinding
-import com.jeong.sesac.sai.util.WeeklyNotesInfo
+import com.jeong.sesac.sai.model.LibraryNoteUI
 
-class WeeklyNoteAdapter(val callBack : (WeeklyNotesInfo) -> Unit) : ListAdapter<WeeklyNotesInfo, WeeklyNoteViewHolder>(
+class WeeklyNoteAdapter(val callBack : (NoteWithUser) -> Unit) : ListAdapter<NoteWithUser, WeeklyNoteViewHolder>(
     DiffUtilWeeklyNotes()
 ){
 
@@ -17,7 +21,10 @@ class WeeklyNoteAdapter(val callBack : (WeeklyNotesInfo) -> Unit) : ListAdapter<
     override fun onBindViewHolder(holder: WeeklyNoteViewHolder, position: Int) {
         val weeklyNoteInfo = currentList[position]
         holder.binding.apply {
-            itemWeeklyNoteImg.setImageResource(weeklyNoteInfo.hint_img)
+            itemWeeklyNoteImg.load(weeklyNoteInfo.image) {
+                crossfade(true)
+                scale(Scale.FILL)
+            }
         root.setOnClickListener {
             callBack(weeklyNoteInfo)
         }
