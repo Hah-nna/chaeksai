3f88a966071a5f81749660b0788927dcdf653684
Hah-nna <hj08hn@gmail.com>
2025-02-03 10:27:57 +0900

    fix: home fragment - mutableState에 따라서 보여주는 부분을 수정합니다 - 쪽지목록을 타입에 따라서 다르게 필터링해서 받아오도록 합니다
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/ui/home/HomeFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/home/HomeFragment.kt
@@ -1,46 +1,63 @@
-package com.jeong.sesac.sai.ui
+package com.jeong.sesac.sai.ui.home
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeong.sesac.domain.model.NoteFilterType
+import com.jeong.sesac.domain.model.SortOrder
 import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.FragmentHomeBinding
-import com.jeong.sesac.sai.recycler.recentlyFoundNote.RecentlyFoundNoteAdapter
+import com.jeong.sesac.sai.model.UiState
+import com.jeong.sesac.sai.recycler.newNote.NewNoteAdapter
 import com.jeong.sesac.sai.recycler.HorizontalDecoration
 import com.jeong.sesac.sai.recycler.weeklyNote.WeeklyNoteAdapter
 import com.jeong.sesac.sai.util.AppPreferenceManager
 import com.jeong.sesac.sai.util.BaseFragment
-import com.jeong.sesac.sai.util.WeeklyNoteMockData
 import com.jeong.sesac.sai.util.throttleFirst
 import com.jeong.sesac.sai.util.throttleTime
+import com.jeong.sesac.sai.viewmodel.NoteListViewModel
+import com.jeong.sesac.sai.viewmodel.WriteNoteViewModel
+import com.jeong.sesac.sai.viewmodel.factory.appViewModelFactory
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import ru.ldralighieri.corbind.view.clicks
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
     private lateinit var weeklyNoteAdapter: WeeklyNoteAdapter
-    private lateinit var recentlyFoundAdapter : RecentlyFoundNoteAdapter
+    private lateinit var recentlyNewAdapter : NewNoteAdapter
     private lateinit var preferenceManger: AppPreferenceManager
+    private val viewModel: NoteListViewModel by viewModels<NoteListViewModel> {
+        appViewModelFactory
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         preferenceManger = AppPreferenceManager.getInstance(requireContext())
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         weeklyNoteAdapter = WeeklyNoteAdapter { weeklyNote ->
-            val action = HomeFragmentDirections.actionFragmentHomeToFragmentWeeklyNoteDetail(weeklyNote)
+            val action = HomeFragmentDirections.actionFragmentHomeToFragmentWeeklyNoteDetail(weeklyNote.id)
             findNavController().navigate(action)
         }
 
-        recentlyFoundAdapter = RecentlyFoundNoteAdapter { foundNote ->
-            val action = HomeFragmentDirections.actionFragmentHomeToFragmentRecentlyFoundNotesDetail(foundNote)
-
+        recentlyNewAdapter = NewNoteAdapter { recentlyNewNote ->
+            val action = HomeFragmentDirections.actionFragmentHomeToFragmentLibraryNoteDetail(recentlyNewNote.id)
             findNavController().navigate(action)
         }
 
@@ -54,7 +71,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
 
             rvRecentlyFoundNotes.apply {
                 layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-                adapter = this@HomeFragment.recentlyFoundAdapter
+                adapter = this@HomeFragment.recentlyNewAdapter
                 addItemDecoration(HorizontalDecoration(requireContext()))
             }
 
@@ -73,15 +90,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
              * 프래그먼트 뷰의 라이프 사이클에 맞춰 코루틴 스코프(ui 스레드)가 연동되어 뷰의 라이프 사이클에 맞게 코루틴을 실행하고 관리하기 위해서
              * */
             tvWeeklyNotesMore.clicks().throttleFirst(throttleTime).onEach {
-                val action = HomeFragmentDirections.actionFragmentHomeToFragmentWeeklyNotes(WeeklyNoteMockData.notesList.first()
-                )
+                val action = HomeFragmentDirections.actionFragmentHomeToFragmentWeeklyNotes()
                 findNavController().navigate(action)
                 Log.d("HOME", "실행")
             }.launchIn(viewLifecycleOwner.lifecycleScope)
 
             tvRecentlyFoundNotesMore.clicks().throttleFirst(throttleTime).onEach {
                 Log.d("HOME", "실행")
-                val action = HomeFragmentDirections.actionFragmentHomeToFragmentRecentlyFoundNotes()
+                val action = HomeFragmentDirections.actionFragmentHomeToFragmentNewNotes()
                 findNavController().navigate(action)
             }.launchIn(viewLifecycleOwner.lifecycleScope)
         }
@@ -98,10 +114,45 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
             throw Error(e.message)
         }
 
-        weeklyNoteAdapter.submitList(WeeklyNoteMockData.notesList)
-        recentlyFoundAdapter.submitList(WeeklyNoteMockData.notesList)
+        viewModel.getNoteList(NoteFilterType.ThisWeek(SortOrder.LATEST))
+        viewModel.getNoteList(NoteFilterType.ByLikes(false))
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.noteListState.collectLatest { state ->
+                    when(state) {
+                        is UiState.Loading -> binding.progressCircle.progressCircular.isVisible = true
+                        is UiState.Success -> {
+                            binding.progressCircle.progressCircular.isVisible = false
+                            weeklyNoteAdapter.submitList(state.data)
+                        }
+                        is UiState.Error -> {
+                            binding.progressCircle.progressCircular.isVisible = false
+                            Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT)
+                        }
+                    }
+
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.noteListState.collectLatest { state ->
+                    when(state) {
+                        is UiState.Loading -> binding.progressCircle.progressCircular.isVisible = true
+                        is UiState.Success -> {
+                            binding.progressCircle.progressCircular.isVisible = false
+                            recentlyNewAdapter.submitList(state.data)
+                        }
+                        is UiState.Error -> {
+                            binding.progressCircle.progressCircular.isVisible = false
+                            Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT)
+                        }
+                    }
+
+                }
+            }
+        }
     }
-
-
-
 }
