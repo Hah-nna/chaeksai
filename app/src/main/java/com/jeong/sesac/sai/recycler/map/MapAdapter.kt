c1fd04c32e5d0a20c247348092e00c59475c18f9
Hah-nna <hj08hn@gmail.com>
2025-02-03 11:03:14 +0900

    fix: kakao map listAdapter @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/map/MapAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/map/MapAdapter.kt
@@ -2,15 +2,34 @@ package com.jeong.sesac.sai.recycler.map
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import com.jeong.sesac.domain.model.PlaceInfo
+import androidx.recyclerview.widget.RecyclerView
+import com.jeong.sesac.feature.model.PlaceInfo
 import com.jeong.sesac.sai.databinding.ItemLibraryInfoBinding
-import com.jeong.sesac.sai.dto.LibraryInfo
+import com.jeong.sesac.sai.util.throttleFirst
+import com.jeong.sesac.sai.util.throttleTime
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.ldralighieri.corbind.view.clicks
 
 class MapAdapter(
     val onRegisterCallback: (PlaceInfo) -> Unit,
-//    val onFindCallback: (LibraryInfo) -> Unit
-) : ListAdapter<PlaceInfo, MapViewHolder>(DiffMap()) {
+    val onLibraryNotesCallback: (PlaceInfo) -> Unit,
+    private val lifecycleScope: LifecycleCoroutineScope
+) : ListAdapter<PlaceInfo, MapAdapter.MapViewHolder>(DiffMap) {
+
+
+    // 여기서 찾기를 눌렀을 때 bottom sheet에 나오는 리사이클러뷰 아이템
+    inner class MapViewHolder(val binding : ItemLibraryInfoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    companion object DiffMap : DiffUtil.ItemCallback<PlaceInfo>() {
+        override fun areItemsTheSame(oldItem: PlaceInfo, newItem: PlaceInfo) = oldItem == newItem
+        override fun areContentsTheSame(oldItem: PlaceInfo, newItem: PlaceInfo) =
+            oldItem.id == newItem.id
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapViewHolder {
         return MapViewHolder(
@@ -22,16 +41,18 @@ class MapAdapter(
         val placeInfoList = currentList[position]
 
         holder.binding.apply {
-            tvLibraryName.setText(placeInfoList.place_name)
-            tvLibraryAddress.setText(placeInfoList.address_name)
-            tvPhone.setText(placeInfoList.phone)
+            tvLibraryName.text = placeInfoList.place
+            tvLibraryAddress.text = placeInfoList.address
+            tvPhone.text = placeInfoList.phone
 
-            btnRegister.setOnClickListener {
+            btnRegister.clicks().throttleFirst(throttleTime).onEach {
                 onRegisterCallback(placeInfoList)
-            }
-//            btnFind.setOnClickListener {
-//                onFindCallback(libraryInfoList)
-//            }
+            }.launchIn(lifecycleScope)
+
+            btnFind.clicks().throttleFirst(throttleTime).onEach {
+                onLibraryNotesCallback(placeInfoList)
+            }.launchIn(lifecycleScope)
+
         }
     }
 }
