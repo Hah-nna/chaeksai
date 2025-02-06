bc437171a4f1e815d2da70977bb7d11b9662df2a
Hah-nna <hj08hn@gmail.com>
2025-02-07 03:18:43 +0900

    fix: comment read, create 관련 함수 추가
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/ui/libraryMap/noteList/LibraryNoteDetailFragment.kt
+++ app/src/main/java/com/jeong/sesac/sai/ui/libraryMap/noteList/LibraryNoteDetailFragment.kt
@@ -3,22 +3,28 @@ package com.jeong.sesac.sai.ui.libraryMap.noteList
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.addCallback
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import coil3.load
 import coil3.request.crossfade
 import coil3.size.Scale
+import com.jeong.sesac.feature.model.Comment
 import com.jeong.sesac.feature.model.NoteWithUser
 import com.jeong.sesac.sai.databinding.FragmentLibraryNoteDetailBinding
+import com.jeong.sesac.sai.model.UiState
 import com.jeong.sesac.sai.recycler.comment.CommentAdapter
+import com.jeong.sesac.sai.util.AppPreferenceManager
 import com.jeong.sesac.sai.util.BaseFragment
+import com.jeong.sesac.sai.util.throttleFirst
+import com.jeong.sesac.sai.util.throttleTime
+import com.jeong.sesac.sai.viewmodel.CommentViewModel
 import com.jeong.sesac.sai.viewmodel.NoteListViewModel
 import com.jeong.sesac.sai.viewmodel.factory.appViewModelFactory
 import kotlinx.coroutines.flow.collectLatest
@@ -30,7 +36,15 @@ import ru.ldralighieri.corbind.view.clicks
 class LibraryNoteDetailFragment : BaseFragment<FragmentLibraryNoteDetailBinding>(FragmentLibraryNoteDetailBinding::inflate){
     private lateinit var commentAdapter : CommentAdapter
     private val args: LibraryNoteDetailFragmentArgs by navArgs()
+    private lateinit var preference : AppPreferenceManager
+
     private val viewModel: NoteListViewModel by activityViewModels<NoteListViewModel> { appViewModelFactory }
+    private val commentViewModel : CommentViewModel by viewModels<CommentViewModel> { appViewModelFactory }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        preference = AppPreferenceManager.getInstance(requireContext())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -59,11 +73,88 @@ class LibraryNoteDetailFragment : BaseFragment<FragmentLibraryNoteDetailBinding>
         }.launchIn(viewLifecycleOwner.lifecycleScope)
 
         viewLifecycleOwner.lifecycleScope.launch {
-           viewModel.selectedNoteState.collectLatest { note ->
-               note?.let { bindData(it) }
+           viewModel.selectedNoteState.collectLatest { state ->
+
+               when (state) {
+                   is UiState.Loading -> binding.progress.progressCircular.isVisible =
+                       true
+                   is UiState.Success -> {
+                       binding.progress.progressCircular.isVisible = false
+                       state.let { it.data?.let { it1 -> bindData(it1) } }
+                   }
+
+                   is UiState.Error -> {
+                       binding.progress.progressCircular.isVisible = false
+                       Toast.makeText(requireContext(), state.error, Toast.LENGTH_SHORT)
+                   }
+               }
            }
         }
+
+        registerComment()
+        getCommentList()
     }
+
+    private fun getCommentList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+                commentViewModel.getComments(preference.nickName, args.noteId)
+
+                commentViewModel.commentListState.collectLatest { state ->
+                    when(state) {
+                        is UiState.Loading -> {
+                            binding.progress.progressCircular.isVisible = true
+                            binding.tvNoComment.isVisible = false
+                        }
+                        is UiState.Success -> {
+                            binding.progress.progressCircular.isVisible = false
+                            commentAdapter.submitList(state.data)
+
+                            binding.tvNoComment.isVisible = state.data.isEmpty()
+                            binding.tvCommentCount.text = "댓글 : ${state.data.size}개"
+                        }
+                        is UiState.Error -> {
+                            binding.progress.progressCircular.isVisible = false
+                            binding.tvNoComment.isVisible = true
+                        }
+                }
+            }
+        }
+    }
+
+    private fun registerComment() {
+        binding.btnRegisterComment.clicks().throttleFirst(throttleTime).onEach {
+            val content = binding.tvComment.text.toString()
+
+
+            if(content.isNotEmpty()) {
+               val comment = Comment(
+                   id = "",
+                   userId = "",
+                   noteId = args.noteId,
+                   content = content,
+                   createdAt = System.currentTimeMillis()
+               )
+
+                commentViewModel.createComment(preference.nickName, args.noteId, comment)
+                binding.tvComment.text?.clear()
+            }
+
+            commentViewModel.commentState.collectLatest {state ->
+                when(state) {
+                    is UiState.Loading -> binding.progress.progressCircular.isVisible = true
+                    is UiState.Success -> {
+                        binding.progress.progressCircular.isVisible = false
+                    }
+                    is UiState.Error ->  {
+                        binding.progress.progressCircular.isVisible = false
+                        Toast.makeText(requireContext(), "에러가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+
 
     private fun bindData(note: NoteWithUser) {
        with(binding) {
@@ -80,10 +171,7 @@ class LibraryNoteDetailFragment : BaseFragment<FragmentLibraryNoteDetailBinding>
            tvLibraryName.text = note.libraryName
            tvTime.text = note.createdAt.toString()
            tvContent.text = note.content
-//           tvCommentCount.text = "댓글 : ${args.libraryNote.comments.size}개"
 
-
-//       commentAdapter.submitList(args.libraryNote.comments)
        }
     }
 }
