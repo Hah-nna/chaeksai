b78b7361060fc9ade1f9879da3f9037637e66c31
Hah-nna <hj08hn@gmail.com>
2025-02-07 03:06:48 +0900

    fix: comment image
    - 코멘트 유저 프로필 이미지를 불러올 때 없어가 null이면 기본 프로필 사진을 보여주게 수정합니다
    
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/comment/CommentAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/comment/CommentAdapter.kt
@@ -5,11 +5,15 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import coil3.load
 import coil3.request.crossfade
+import coil3.request.error
+import coil3.request.fallback
 import coil3.size.Scale
+import com.jeong.sesac.feature.model.CommentWithUser
+import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.ItemCommentBinding
 import com.jeong.sesac.sai.model.CommentUI
 
-class CommentAdapter(val callback : (CommentUI) -> Unit) : ListAdapter<CommentUI, CommentViewHolder>(DiffComment()) {
+class CommentAdapter(val callback : (CommentWithUser) -> Unit) : ListAdapter<CommentWithUser, CommentViewHolder>(DiffComment()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         return CommentViewHolder(ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -23,10 +27,13 @@ class CommentAdapter(val callback : (CommentUI) -> Unit) : ListAdapter<CommentUI
             ivProfile.load(commentList.userInfo.profile) {
                 crossfade(true)
                 scale(Scale.FILL)
+                fallback(R.drawable.ic_default_profile)
+                error(R.drawable.ic_default_profile)
+
             }
             tvNickname.text = commentList.userInfo.nickName
             tvContent.text = commentList.content
-            tvTime.text = commentList.createdAt
+            tvTime.text = commentList.createdAt.toString()
         }
     }
 }
