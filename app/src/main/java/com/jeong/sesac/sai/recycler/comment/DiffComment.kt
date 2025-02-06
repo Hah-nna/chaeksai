46afee4d175fefd115df606addc98a512de161ee
Hah-nna <hj08hn@gmail.com>
2025-02-07 03:12:45 +0900

    fix: data type change
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/comment/DiffComment.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/comment/DiffComment.kt
@@ -1,13 +1,14 @@
 package com.jeong.sesac.sai.recycler.comment
 
 import androidx.recyclerview.widget.DiffUtil
+import com.jeong.sesac.feature.model.CommentWithUser
 import com.jeong.sesac.sai.model.CommentUI
 
-class DiffComment : DiffUtil.ItemCallback<CommentUI>() {
+class DiffComment : DiffUtil.ItemCallback<CommentWithUser>() {
 
-    override fun areItemsTheSame(oldItem: CommentUI, newItem: CommentUI): Boolean = oldItem == newItem
+    override fun areItemsTheSame(oldItem: CommentWithUser, newItem: CommentWithUser): Boolean = oldItem == newItem
 
-    override fun areContentsTheSame(oldItem: CommentUI, newItem: CommentUI): Boolean {
+    override fun areContentsTheSame(oldItem: CommentWithUser, newItem: CommentWithUser): Boolean {
         return oldItem.id == newItem.id
     }
 }
