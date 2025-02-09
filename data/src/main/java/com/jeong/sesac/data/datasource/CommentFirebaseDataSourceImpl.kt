5bbe660fead135fd05b3faa26c5cbbe050b3c43a
Hah-nna <hj08hn@gmail.com>
2025-02-10 04:32:59 +0900

    feat: update, delete추가
    @by Hah-nna

--- data/src/main/java/com/jeong/sesac/data/datasource/CommentFirebaseDataSourceImpl.kt
+++ data/src/main/java/com/jeong/sesac/data/datasource/CommentFirebaseDataSourceImpl.kt
@@ -9,12 +9,17 @@ import com.jeong.sesac.feature.model.CommentWithUser
 import com.jeong.sesac.feature.model.Note
 import kotlinx.coroutines.tasks.await
 
-class CommentFirebaseDataSourceImpl(private val firebaseDataSource: FireBaseDataSourceImpl) : CommentFirebaseDataSource {
+class CommentFirebaseDataSourceImpl(private val firebaseDataSource: FireBaseDataSourceImpl) :
+    CommentFirebaseDataSource {
     private val noteCollectionRef = Firebase.firestore.collection("notes")
 
 
-    override suspend fun createComment(nickname: String, noteId: String, comment: Comment) : Boolean {
-       return runCatching {
+    override suspend fun createComment(
+        nickname: String,
+        noteId: String,
+        comment: Comment
+    ): Boolean {
+        return runCatching {
             val commentRef = noteCollectionRef
                 .document(noteId)
                 .collection("comments")
@@ -36,50 +41,70 @@ class CommentFirebaseDataSourceImpl(private val firebaseDataSource: FireBaseData
                 .document(commentRef.id)
                 .update("userId", userId)
                 .await()
-           true
+            true
         }.getOrElse { e ->
-           Log.e("코멘트생성에러", "코멘트생성에러: ${e.message}")
-           false
-       }
+            Log.e("코멘트생성에러", "코멘트생성에러: ${e.message}")
+            false
+        }
     }
 
-    override suspend fun getComments(nickname: String, noteId: String): List<CommentWithUser> {
+    override suspend fun getComments(nickname: String, noteId: String): Result<List<CommentWithUser>> {
         return runCatching {
-           noteCollectionRef
+            noteCollectionRef
                 .document(noteId)
                 .collection("comments")
                 .orderBy("createdAt", Query.Direction.DESCENDING)
                 .get()
                 .await()
-               .documents.mapNotNull { it ->
+                .documents.mapNotNull { it ->
+                    val userId = firebaseDataSource.getIdByNickname(nickname)
 
-                   val userId = firebaseDataSource.getIdByNickname(nickname)
+                    val comment = it.toObject(CommentWithUser::class.java)
+                    comment?.let {
+                        val userInfo = firebaseDataSource.getUserInfo(userId!!)
 
-                   val comment = it.toObject(CommentWithUser::class.java)
-                   comment?.let {
-                      val userInfo = firebaseDataSource.getUserInfo(userId!!)
-                   Log.d("userinfo", "${ userInfo.id }, ${userInfo.nickName}, ${userInfo.profile}")
+                        CommentWithUser(
+                            id = it.id,
+                            userInfo = userInfo,
+                            content = it.content,
+                            createdAt = it.createdAt
+                        )
+                    }
+                }
 
-                       CommentWithUser(
-                           id = it.id,
-                           userInfo = userInfo,
-                           content = it.content,
-                           createdAt = it.createdAt
-                       )
-                   }
-               }
-
-        }.getOrElse { e ->
+        }.onFailure { e ->
             Log.e("코멘트가져오기실패", "코멘트가져오기실패: ${e.message}")
-            emptyList()
+            "코멘트 가져오기 실패"
         }
     }
 
-    override suspend fun UpdateComment() {
-        TODO("Not yet implemented0")
+    override suspend fun updateComment(
+        noteId: String,
+        commentId: String,
+        content: String
+    ): Result<Unit> {
+        return runCatching {
+            noteCollectionRef.document(noteId)
+                .collection("comments")
+                .document(commentId)
+                .update("content", content)
+                .await()
+        }.map { Unit }.onFailure { e ->
+            Log.e("코멘트 업데이트실패", "${e.message}")
+        }
     }
 
-    override suspend fun DeleteComment() {
-        TODO("Not yet implemented")
+    override suspend fun deleteComment(noteId: String, commentId: String): Result<Unit> {
+        return runCatching {
+            noteCollectionRef.document(noteId)
+                .collection("comments")
+                .document(commentId)
+                .delete()
+                .await()
+
+        }.map { Unit }
+            .onFailure { e ->
+                Log.e("코멘트 삭제실패", "${e.message}")
+            }
     }
 }
