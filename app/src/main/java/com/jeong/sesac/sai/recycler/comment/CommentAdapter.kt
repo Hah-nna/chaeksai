46c41e28e1894cda974c7d9c4bff0708acf44ff3
Hah-nna <hj08hn@gmail.com>
2025-02-10 04:32:15 +0900

    feat: menu 추가
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/comment/CommentAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/comment/CommentAdapter.kt
@@ -2,6 +2,7 @@ package com.jeong.sesac.sai.recycler.comment
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.recyclerview.widget.ListAdapter
 import coil3.load
 import coil3.request.crossfade
@@ -11,9 +12,15 @@ import coil3.size.Scale
 import com.jeong.sesac.feature.model.CommentWithUser
 import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.ItemCommentBinding
-import com.jeong.sesac.sai.model.CommentUI
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import ru.ldralighieri.corbind.view.clicks
 
-class CommentAdapter(val callback : (CommentWithUser) -> Unit) : ListAdapter<CommentWithUser, CommentViewHolder>(DiffComment()) {
+class CommentAdapter(
+    val nickname: String,
+    val lifecycleScope: LifecycleCoroutineScope ,
+    val callback : (CommentWithUser, Boolean) -> Unit
+) : ListAdapter<CommentWithUser, CommentViewHolder>(DiffComment()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         return CommentViewHolder(ItemCommentBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -21,19 +28,24 @@ class CommentAdapter(val callback : (CommentWithUser) -> Unit) : ListAdapter<Com
     }
 
     override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
-        val commentList = currentList[position]
+        val comment = currentList[position]
         holder.binding.apply {
 
-            ivProfile.load(commentList.userInfo.profile) {
+            ivProfile.load(comment.userInfo.profile) {
                 crossfade(true)
                 scale(Scale.FILL)
                 fallback(R.drawable.ic_default_profile)
                 error(R.drawable.ic_default_profile)
 
             }
-            tvNickname.text = commentList.userInfo.nickName
-            tvContent.text = commentList.content
-            tvTime.text = commentList.createdAt.toString()
+            tvNickname.text = comment.userInfo.nickName
+            tvContent.text = comment.content
+            tvTime.text = comment.createdAt.toString()
+
+            ivMenu.clicks().onEach {
+                val isCommentUser = comment.userInfo.nickName == nickname
+                callback(comment, isCommentUser)
+            }.launchIn(lifecycleScope)
         }
     }
 }
