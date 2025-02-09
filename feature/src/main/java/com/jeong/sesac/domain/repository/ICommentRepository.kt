87f530441d7c4462f2d143d52af3334453f99749
Hah-nna <hj08hn@gmail.com>
2025-02-10 04:23:31 +0900

    fix: update, delete 추가
    @by Hah-nna

--- feature/src/main/java/com/jeong/sesac/domain/repository/ICommentRepository.kt
+++ feature/src/main/java/com/jeong/sesac/domain/repository/ICommentRepository.kt
@@ -5,5 +5,7 @@ import com.jeong.sesac.feature.model.CommentWithUser
 
 interface ICommentRepository {
     suspend fun createComment(nickname: String, noteId: String, comment: Comment): Boolean
-    suspend fun getComments(nickname: String, noteId: String): List<CommentWithUser>
+    suspend fun getComments(nickname: String, noteId: String): Result<List<CommentWithUser>>
+    suspend fun updateComment(noteId: String, commentId: String, content: String): Result<Unit>
+    suspend fun deleteComment(noteId: String, commentId: String): Result<Unit>
 }
