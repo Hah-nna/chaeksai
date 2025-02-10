be3091eb0725d8c4d1cb29403fcfd9964719d421
Hah-nna <hj08hn@gmail.com>
2025-02-10 16:12:50 +0900

    fix: weiteNoteViewModel -> NoteViewModel로 뷰모델 이름 변경
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
+++ app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
@@ -1,28 +1,23 @@
 package com.jeong.sesac.sai.viewmodel.factory
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.jeong.sesac.data.api.manager.KakaoMapManager
-import com.jeong.sesac.data.api.manager.OpenAPIManager
 import com.jeong.sesac.data.datasource.CommentFirebaseDataSourceImpl
 import com.jeong.sesac.data.datasource.FireBaseDataSourceImpl
 import com.jeong.sesac.data.datasource.FireBaseStorageDataSourceImpl
 import com.jeong.sesac.data.datasource.KakaoMapDataSourceImpl
-import com.jeong.sesac.data.datasource.OpenAPIDataSourceImpl
 import com.jeong.sesac.data.repository.CommentRepositoryImpl
 import com.jeong.sesac.data.repository.KakaoMapRepositoryImpl
 import com.jeong.sesac.data.repository.LoginRepositoryImpl
 import com.jeong.sesac.data.repository.NoteListRepositoryImpl
-import com.jeong.sesac.data.repository.OpenApiRepositoryImpl
-import com.jeong.sesac.data.repository.WriteNoteRepositoryImpl
-import com.jeong.sesac.sai.util.AppPreferenceManager
+import com.jeong.sesac.data.repository.NoteRepositoryImpl
 import com.jeong.sesac.sai.util.ChakSaiClass
 import com.jeong.sesac.sai.viewmodel.CommentViewModel
 import com.jeong.sesac.sai.viewmodel.KakaoMapViewModel
 import com.jeong.sesac.sai.viewmodel.LoginViewModel
 import com.jeong.sesac.sai.viewmodel.NoteListViewModel
-import com.jeong.sesac.sai.viewmodel.WriteNoteViewModel
+import com.jeong.sesac.sai.viewmodel.NoteViewModel
 
 @Suppress("UNCHECKED_CAST")
 val appViewModelFactory = object : ViewModelProvider.Factory {
@@ -35,8 +30,8 @@ val appViewModelFactory = object : ViewModelProvider.Factory {
                     val kakaoService = KakaoMapManager.getInstance()
                     KakaoMapViewModel(KakaoMapRepositoryImpl(KakaoMapDataSourceImpl(kakaoService)))
                 }
-                isAssignableFrom(WriteNoteViewModel::class.java) -> {
-                    WriteNoteViewModel(WriteNoteRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext()))))
+                isAssignableFrom(NoteViewModel::class.java) -> {
+                    NoteViewModel(NoteRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext()))))
                 }
 
                 isAssignableFrom(NoteListViewModel::class.java) -> {
