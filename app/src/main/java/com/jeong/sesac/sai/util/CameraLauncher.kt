15e07041b9e24e0fb2b96213ce1c9f2d5c505ec6
Hah-nna <hj08hn@gmail.com>
2025-02-10 16:12:15 +0900

    fix: 카메라 헬퍼 클래스 생성 - 카메라를 다른 곳에서도 재활용하기 위해 카메라 헬퍼 클래스 생성
    @by Hah-nna

---
+++ app/src/main/java/com/jeong/sesac/sai/util/CameraLauncher.kt
@@ -0,0 +1,43 @@
+package com.jeong.sesac.sai.util
+
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.jeong.sesac.sai.CameraActivity
+import com.jeong.sesac.sai.CameraMode
+
+
+class CameraLauncher(private val fragment: Fragment,
+                     private val onTakePhoto: (Uri?) -> Unit) {
+
+    /**
+     *
+     * cameraActivity의 onImageSaved()의 setResult()에서 온 결과가
+     * cameraLauncher의 콜백으로 전달됨
+     *
+     * 힌트이미지를 찍고나서 결과 받아오는 함수
+     * */
+    private val photoCameraLauncher = fragment.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        // result.data.data : CameraActivity가 리턴한 Intent
+        if (result.resultCode == RESULT_OK) {
+            onTakePhoto(result.data?.data)
+        }
+    }
+
+    // 힌트 이미지 찍을 때와 isbn을 스캔할 때 각각 다르게 카메라를 launch하는 함수
+    fun startCameraLauncher(context: Context, mode: CameraMode) {
+        val intent = Intent(context, CameraActivity::class.java).apply {
+            putExtra("camera_mode", mode.name)
+        }
+        if (mode == CameraMode.PHOTO_CAPTURE) {
+            photoCameraLauncher.launch(intent)
+        }
+    }
+
+
+}
