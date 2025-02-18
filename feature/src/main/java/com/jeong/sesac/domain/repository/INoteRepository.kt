452c2385756fb560559444f37abf7158f78d7dab
Hah-nna <hj08hn@gmail.com>
2025-02-18 13:38:59 +0900

    fix: 파라미터 중 nickname 삭제, 리턴타입 변경 @by Hah-nna

--- feature/src/main/java/com/jeong/sesac/domain/repository/INoteRepository.kt
+++ feature/src/main/java/com/jeong/sesac/domain/repository/INoteRepository.kt
@@ -3,5 +3,5 @@ package com.jeong.sesac.domain.repository
 import com.jeong.sesac.feature.model.Note
 
 interface INoteRepository{
-    suspend fun createNote(note: Note, nickname: String): Boolean
+    suspend fun createNote(note: Note): Result<Boolean>
 }
