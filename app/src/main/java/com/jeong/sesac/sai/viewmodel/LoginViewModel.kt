a17615295998695972fcf26e73ad563368bb7441
Hah-nna <hj08hn@gmail.com>
2025-02-18 13:47:41 +0900

    fix: _duplicateState 리턴값 수정 @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/jeong/sesac/sai/viewmodel/LoginViewModel.kt
@@ -10,26 +10,31 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class LoginViewModel(private val loginRepo: LoginRepositoryImpl) : ViewModel() {
-    private var _duplicateState = MutableStateFlow<UiState<String>>(UiState.Loading)
+    private var _duplicateState = MutableStateFlow<UiState<Boolean>>(UiState.Loading)
     val duplicateState get() = _duplicateState.asStateFlow()
 
     private val _userCreateState = MutableStateFlow<UiState<String>>(UiState.Loading)
     val userCreateState = _userCreateState.asStateFlow()
 
     @SuppressLint("NewApi")
-    fun serUserInfo(nickname: String) = viewModelScope.launch {
+    fun setUserInfo(nickname: String) = viewModelScope.launch {
         _userCreateState.value = UiState.Loading
-        val isSuccess = loginRepo.setUser(nickname)
-        _userCreateState.value =
-            if (isSuccess) UiState.Success(nickname) else UiState.Error("다시 시도해주세요")
+        loginRepo.setUser(nickname)
+            .onSuccess {
+                _userCreateState.value = UiState.Success(it)
+            }.onFailure {
+                UiState.Error("다시 시도해주세요")
+            }
     }
 
     fun checkDuplicatedNickname(nickname: String) = viewModelScope.launch {
         _duplicateState.value = UiState.Loading
-        val isSuccess = loginRepo.checkDuplicateNickname(nickname)
-        _duplicateState.value =
-            if (isSuccess) UiState.Success(nickname) else UiState.Error("다시 시도해주세요")
+        loginRepo.checkDuplicateNickname(nickname)
+            .onSuccess {
+                _duplicateState.value = UiState.Success(it)
+            }.onFailure {
+                _duplicateState.value = UiState.Error("다시 시도해주세요")
+            }
     }
-
 }
 
