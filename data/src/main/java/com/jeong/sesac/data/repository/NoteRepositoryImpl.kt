77eda0cbd0373b4e4afbce4bcd79b1aa7c456b36
Hah-nna <hj08hn@gmail.com>
2025-02-25 20:22:27 +0900

    fix: getNote 수정 @by Hah-nna

--- data/src/main/java/com/jeong/sesac/data/repository/NoteRepositoryImpl.kt
+++ data/src/main/java/com/jeong/sesac/data/repository/NoteRepositoryImpl.kt
@@ -4,10 +4,14 @@ import com.jeong.sesac.data.datasource.FireBaseDataSource
 import com.jeong.sesac.domain.repository.INoteRepository
 import com.jeong.sesac.feature.model.Note
 import com.jeong.sesac.feature.model.NoteWithUser
+import com.jeong.sesac.feature.model.UserInfo
 
-class NoteRepositoryImpl(private val fireBaseDataSource: FireBaseDataSource) : INoteRepository {
+class NoteRepositoryImpl(
+    private val fireBaseDataSource: FireBaseDataSource,
+    private val UserRepo: UserRepositoryImpl
+) : INoteRepository {
 
-    override suspend fun createNote(note: Note): Result<Boolean>{
+    override suspend fun createNote(note: Note): Result<Boolean> {
         val noteInfo = Note(
             id = "",
             userId = note.userId,
@@ -16,8 +20,8 @@ class NoteRepositoryImpl(private val fireBaseDataSource: FireBaseDataSource) : I
             content = note.content,
             createdAt = note.createdAt,
             libraryName = note.libraryName,
-            likes = 0,
-            )
+            likes = emptyList(),
+        )
 
         return fireBaseDataSource.createNote(noteInfo)
     }
@@ -26,8 +30,26 @@ class NoteRepositoryImpl(private val fireBaseDataSource: FireBaseDataSource) : I
         return fireBaseDataSource.updateNote(noteId, note)
     }
 
-    suspend fun getNote(noteId: String): Result<NoteWithUser> {
-        return fireBaseDataSource.getNote(noteId)
+    suspend fun getNote(noteId: String, userId: String): Result<NoteWithUser> {
+        return fireBaseDataSource.getNote(noteId).map { note ->
+            val userInfo = UserRepo.getUserInfo(userId)
+            NoteWithUser(
+                id = note.id,
+                userInfo = UserInfo(
+                    id = userInfo.id,
+                    profile = userInfo.profile,
+                    nickName = userInfo.nickName
+                ),
+                image = note.image,
+                title = note.title,
+                content = note.content,
+                createdAt = note.createdAt,
+                libraryName = note.libraryName,
+                likes = note.likes
+            )
+        }.onFailure {
+            throw Exception("${it.message}")
+        }
     }
 
     suspend fun deleteNote(noteId: String): Result<Unit> {
