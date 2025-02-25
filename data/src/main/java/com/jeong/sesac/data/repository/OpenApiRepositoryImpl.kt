b46c97d6618636ae72de7da9f00cfd968de7adfa
Hah-nna <hj08hn@gmail.com>
2025-02-25 20:20:07 +0900

    fix: return type 변경 @by Hah-nna

--- data/src/main/java/com/jeong/sesac/data/repository/OpenApiRepositoryImpl.kt
+++ data/src/main/java/com/jeong/sesac/data/repository/OpenApiRepositoryImpl.kt
@@ -7,15 +7,7 @@ import com.jeong.sesac.feature.repository.IOpenApiRepository
 class OpenApiRepositoryImpl(private val openApiDataSource : OpenAPIDataSourceImpl) :
     IOpenApiRepository {
 
-    override suspend fun getBookInfo(isbn: String): List<BookInfo> {
-        val result = openApiDataSource.getBookInfo(isbn).map {
-            BookInfo(
-                it.page_no,
-                it.title,
-                it.isbn,
-                it.book_img
-            )
-        }
-        return result
+    override suspend fun getBookInfo(isbn: String): Result<String> {
+        return openApiDataSource.getBookInfo(isbn)
     }
 }
