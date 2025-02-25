0882388d0251f007c9d2a061186a85eab4b9743a
Hah-nna <hj08hn@gmail.com>
2025-02-25 20:19:47 +0900

    fix: book view model 추가 @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
+++ app/src/main/java/com/jeong/sesac/sai/viewmodel/factory/viewModelFatory.kt
@@ -3,17 +3,24 @@ package com.jeong.sesac.sai.viewmodel.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.jeong.sesac.data.api.manager.KakaoMapManager
+import com.jeong.sesac.data.api.manager.OpenAPIManager
+import com.jeong.sesac.data.datasource.BookDataSourceImpl
 import com.jeong.sesac.data.datasource.CommentFirebaseDataSourceImpl
 import com.jeong.sesac.data.datasource.FireBaseDataSourceImpl
 import com.jeong.sesac.data.datasource.FireBaseStorageDataSourceImpl
 import com.jeong.sesac.data.datasource.KakaoMapDataSourceImpl
+import com.jeong.sesac.data.datasource.OpenAPIDataSourceImpl
+import com.jeong.sesac.data.repository.BookRepositoryImpl
 import com.jeong.sesac.data.repository.CommentRepositoryImpl
 import com.jeong.sesac.data.repository.KakaoMapRepositoryImpl
 import com.jeong.sesac.data.repository.LBSRepositoryImpl
 import com.jeong.sesac.data.repository.LoginRepositoryImpl
 import com.jeong.sesac.data.repository.NoteListRepositoryImpl
 import com.jeong.sesac.data.repository.NoteRepositoryImpl
+import com.jeong.sesac.data.repository.OpenApiRepositoryImpl
+import com.jeong.sesac.data.repository.UserRepositoryImpl
 import com.jeong.sesac.sai.util.ChakSaiClass
+import com.jeong.sesac.sai.viewmodel.BookViewModel
 import com.jeong.sesac.sai.viewmodel.CommentViewModel
 import com.jeong.sesac.sai.viewmodel.KakaoMapViewModel
 import com.jeong.sesac.sai.viewmodel.LoginViewModel
@@ -22,25 +29,34 @@ import com.jeong.sesac.sai.viewmodel.NoteViewModel
 
 @Suppress("UNCHECKED_CAST")
 val appViewModelFactory = object : ViewModelProvider.Factory {
+    val context = ChakSaiClass.getContext()
     override fun < T : ViewModel>  create(modelClass: Class<T>): T =
         with(modelClass) {
             when {
                 isAssignableFrom(LoginViewModel::class.java) ->
-                    LoginViewModel(LoginRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext()))))
+                    LoginViewModel(LoginRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context))))
                 isAssignableFrom(KakaoMapViewModel::class.java) -> {
                     val kakaoService = KakaoMapManager.getInstance()
-                    KakaoMapViewModel(KakaoMapRepositoryImpl(KakaoMapDataSourceImpl(kakaoService)), LBSRepositoryImpl(ChakSaiClass.getContext()))
+                    KakaoMapViewModel(KakaoMapRepositoryImpl(KakaoMapDataSourceImpl(kakaoService)), LBSRepositoryImpl(context))
                 }
                 isAssignableFrom(NoteViewModel::class.java) -> {
-                    NoteViewModel(NoteRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext()))))
+                    NoteViewModel(NoteRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context)),  UserRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context)))))
                 }
 
                 isAssignableFrom(NoteListViewModel::class.java) -> {
-                    NoteListViewModel(NoteListRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext()))))
+                    NoteListViewModel(NoteListRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context)),  UserRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context)))))
                 }
 
                 isAssignableFrom(CommentViewModel::class.java) -> {
-                    CommentViewModel(CommentRepositoryImpl(CommentFirebaseDataSourceImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(ChakSaiClass.getContext())))))
+                    CommentViewModel(CommentRepositoryImpl(CommentFirebaseDataSourceImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context))), UserRepositoryImpl(FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context)))))
+                }
+                isAssignableFrom(BookViewModel::class.java) -> {
+                    val openApiService = OpenAPIManager.getInstance()
+                    BookViewModel((BookRepositoryImpl(BookDataSourceImpl(openApiService), UserRepositoryImpl(
+                        FireBaseDataSourceImpl(FireBaseStorageDataSourceImpl(context))
+                    ))), OpenApiRepositoryImpl(
+                        OpenAPIDataSourceImpl(openApiService)
+                    ))
                 }
                 else ->
                     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
