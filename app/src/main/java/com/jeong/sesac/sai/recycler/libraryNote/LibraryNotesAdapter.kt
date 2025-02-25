8417f612e443fc6b2a04d9d151d9f44284f8273b
Hah-nna <hj08hn@gmail.com>
2025-02-25 20:13:42 +0900

    fix: 좋아요를 isLiked에 따라서 ui 변경, clearTotalLikes, getTotalLikes 추가,
    - likedNotesState라는 map에 포스팅 아이디와 좋아요가 눌렸는지 아닌지를 저장해서 나중에 한꺼번에 좋아요 업데이트
    @by Hah-nna

--- app/src/main/java/com/jeong/sesac/sai/recycler/libraryNote/LibraryNotesAdapter.kt
+++ app/src/main/java/com/jeong/sesac/sai/recycler/libraryNote/LibraryNotesAdapter.kt
@@ -1,5 +1,6 @@
 package com.jeong.sesac.sai.recycler.libraryNote
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleCoroutineScope
@@ -9,6 +10,7 @@ import coil3.request.crossfade
 import coil3.request.error
 import coil3.request.fallback
 import coil3.size.Scale
+import coil3.size.Size
 import com.jeong.sesac.feature.model.NoteWithUser
 import com.jeong.sesac.sai.R
 import com.jeong.sesac.sai.databinding.ItemLibraryNoteBinding
@@ -19,8 +21,13 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import ru.ldralighieri.corbind.view.clicks
 
-class LibraryNotesAdapter(val lifecycleScope: LifecycleCoroutineScope, val callBack: (NoteWithUser) -> Unit) :
-ListAdapter<NoteWithUser, LibraryNoteViewHolder>(DiffLibraryNote()){
+class LibraryNotesAdapter(
+    val userId: String,
+    val lifecycleScope: LifecycleCoroutineScope,
+    val callBack: (NoteWithUser) -> Unit,
+) : ListAdapter<NoteWithUser, LibraryNoteViewHolder>(DiffLibraryNote()){
+
+    private val likedNotesState = mutableMapOf<String, Boolean>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryNoteViewHolder {
         return LibraryNoteViewHolder(
@@ -29,29 +36,62 @@ ListAdapter<NoteWithUser, LibraryNoteViewHolder>(DiffLibraryNote()){
     }
 
     override fun onBindViewHolder(holder: LibraryNoteViewHolder, position: Int) {
-        val libraryNoteList = currentList[position]
+        val libraryNote = currentList[position]
+
+
         holder.binding.apply {
-            ivProfile.load(libraryNoteList.userInfo.profile) {
+
+            val initialLikeState = libraryNote.likes.contains(userId)
+            val currentLikeState = likedNotesState[libraryNote.id]
+
+            val isLiked = if(currentLikeState != null) currentLikeState else initialLikeState
+
+            val likeCount = if (isLiked != initialLikeState) {
+                libraryNote.likes.size + (if (isLiked) 1 else -1)
+            } else {
+                libraryNote.likes.size
+            }
+
+
+            ivProfile.load(libraryNote.userInfo.profile) {
                 crossfade(true)
                 scale(Scale.FILL)
                 fallback(R.drawable.ic_default_profile)
                 error(R.drawable.ic_default_profile)
             }
 
-            ivImg.load(libraryNoteList.image) {
+            ivImg.load(libraryNote.image) {
                 crossfade(true)
+                size(Size.ORIGINAL)
                 scale(Scale.FILL)
                 fallback(R.drawable.ic_default_profile)
                 error(R.drawable.ic_default_profile)
             }
-            tvNickname.text = libraryNoteList.userInfo.nickName
-            tvLibraryName.text = libraryNoteList.libraryName
-            tvTitle.text = libraryNoteList.title
-            tvTime.text = libraryNoteList.createdAt.toTimeConverter()
+
+            ivLike.setImageResource(
+                if(isLiked) R.drawable.ic_filled_heart
+                else R.drawable.ic_empty_heart
+            )
+
+            tvNickname.text = libraryNote.userInfo.nickName
+            tvLibraryName.text = libraryNote.libraryName
+            tvTitle.text = libraryNote.title
+            tvLikeCount.text = likeCount.toString()
+            tvTime.text = libraryNote.createdAt.toTimeConverter()
             root.clicks().throttleFirst(throttleTime).onEach {
-                callBack(libraryNoteList)
+                callBack(libraryNote)
+            }.launchIn(lifecycleScope)
+            ivLike.clicks().throttleFirst(throttleTime).onEach {
+
+                val newLikeState = !isLiked
+
+                likedNotesState[libraryNote.id] = newLikeState
+                notifyItemChanged(position)
+                Log.d("newLikeState", "${newLikeState}")
             }.launchIn(lifecycleScope)
         }
-
     }
+
+    fun getTotalLikes() = likedNotesState
+    fun clearTotalLikes() = likedNotesState.clear()
 }
