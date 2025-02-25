0ebae92973fbdc89088db0ac315c1a6fd512da74
Hah-nna <hj08hn@gmail.com>
2025-02-25 20:02:59 +0900

    fix: 필드 추가 및 book review 추가 @by Hah-nna

--- feature/src/main/java/com/jeong/sesac/domain/model/BookInfo.kt
+++ feature/src/main/java/com/jeong/sesac/domain/model/BookInfo.kt
@@ -1,8 +1,38 @@
 package com.jeong.sesac.feature.model
 
+
 data class BookInfo (
-    val page_no : Int = 1,
-    val title : String,
-    val isbn : String,
-    val book_img : String
+    val id: String = "",
+    val userId: String = "",
+    val createdAt: Long = System.currentTimeMillis(),
+    val library: String = "",
+    val title : String = "",
+    val isbn : String= "",
+    val book_img : String = "",
+    val author: String = "",
+    val introduction: String = "",
+    val publisher: String = "",
+    val form: String = "",
+    val publish_date: String = "",
+    val score: Float = 0f,
+    val page: String = ""
+)
+
+
+data class BookReview (
+    val id: String = "",
+    val userId: String = "",
+    val content: String = "",
+    val score: Float = 0F,
+    val createdAt: Long = 0L,
+    val library: String = "",
+)
+
+data class BookReviewWithUser (
+    val id: String,
+    val userInfo: UserInfo,
+    val content: String,
+    val score: Float,
+    val createdAt: Long,
+    val library: String,
 )
